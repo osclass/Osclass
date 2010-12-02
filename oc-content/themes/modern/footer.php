42791d894169dfe1bf277bb167688d0be9287ff7
conejo-osclass <desteban@osclass.org>
2010-12-02 10:29:41 +0100

    issue 249

--- oc-content/themes/modern/footer.php
+++ oc-content/themes/modern/footer.php
@@ -2,7 +2,7 @@
 
             </div> <!-- content -->
             <div id="footer">
-                <?php _e('This web is proudly using the <a title="OSClass web" href="http://osclass.org/">open source classifieds</a> software <strong>OSClass</strong>.'); ?>
+                <?php _e('This website is proudly using the <a title="OSClass web" href="http://osclass.org/">open source classifieds</a> software <strong>OSClass</strong>.'); ?>
             </div>
         </div> <!-- container -->
         <?php osc_runHook('footer'); ?>
--- oc-content/themes/modern_vertical/footer.php
+++ oc-content/themes/modern_vertical/footer.php
@@ -4,7 +4,7 @@
 
             </div> <!-- content -->
             <div id="footer">
-                <?php _e('This web is proudly using the <a title="OSClass web" href="http://osclass.org/">open source classifieds</a> software <strong>OSClass</strong>.'); ?>
+                <?php _e('This website is proudly using the <a title="OSClass web" href="http://osclass.org/">open source classifieds</a> software <strong>OSClass</strong>.'); ?>
             </div>
         </div> <!-- container -->
     </body>
