16336d9212529988ffb8c8f77b7828bee21c9f57
conejo-osclass <desteban@osclass.org>
2011-01-10 20:14:26 +0100

    issues 281 & 282

--- oc-includes/osclass/frm/Category.form.class.php
+++ oc-includes/osclass/frm/Category.form.class.php
@@ -31,7 +31,7 @@ class CategoryForm extends Form {
                 echo '<option value="">' . $default_item . '</option>' ;
             }
             foreach($categories as $c) {
-                echo '<option value="' . $c['pk_i_id'] . '"' . ( ($category == $c['pk_i_id']) ? 'selected="selected"' : '' ) . '>' . $c['s_name'] . '</option>' ;
+                echo '<option value="' . $c['pk_i_id'] . '"' . ( ($category['pk_i_id'] == $c['pk_i_id']) ? 'selected="selected"' : '' ) . '>' . $c['s_name'] . '</option>' ;
                 if(isset($c['categories']) && is_array($c['categories'])) {
                     CategoryForm::subcategory_select($c['categories'], $category, $default_item, 1);
                 }
@@ -47,7 +47,50 @@ class CategoryForm extends Form {
             }
             $deep++;
             foreach($categories as $c) {
-                echo '<option value="' . $c['pk_i_id'] . '"' . ( ($category == $c['pk_i_id']) ? 'selected="selected"' : '' ) . '>' . $deep_string.$c['s_name'] . '</option>' ;
+                echo '<option value="' . $c['pk_i_id'] . '"' . ( ($category['pk_i_id'] == $c['pk_i_id']) ? 'selected="selected"' : '' ) . '>' . $deep_string.$c['s_name'] . '</option>' ;
+                if(isset($c['categories']) && is_array($c['categories'])) {
+                    CategoryForm::subcategory_select($c['categories'], $category, $default_item, $deep+1);
+                }
+            }
+    }
+    
+    static public function parent_category_select($categories, $category, $default_item = null) {
+        echo '<select name="fk_i_parent_id" id="parentId">' ;
+            if(isset($default_item)) {
+                echo '<option value="">' . $default_item . '</option>' ;
+            }
+            foreach($categories as $c) {
+                if($category['fk_i_parent_id']==$c['pk_i_id']) {
+                    $extra = ' selected="selected" ';
+                } else if($category['pk_i_id']==$c['pk_i_id']) {
+                    $extra = ' disabled="disabled" ';
+                } else {
+                    $extra = '';
+                }
+                echo '<option value="' . $c['pk_i_id'] . '"' . $extra . '>' . $c['s_name'] . '</option>' ;
+                if(isset($c['categories']) && is_array($c['categories'])) {
+                    CategoryForm::parent_subcategory_select($c['categories'], $category, $default_item, 1);
+                }
+            }
+        echo '</select>' ;
+        return true ;
+    }
+
+    static public function parent_subcategory_select($categories, $category, $default_item = null, $deep = 0) {
+            $deep_string = "";
+            for($var = 0;$var<$deep;$var++) {
+                $deep_string .= '&nbsp;&nbsp;';
+            }
+            $deep++;
+            foreach($categories as $c) {
+                if($category['fk_i_parent_id']==$c['pk_i_id']) {
+                    $extra = ' selected="selected" ';
+                } else if($category['pk_i_id']==$c['pk_i_id']) {
+                    $extra = ' disabled="disabled" ';
+                } else {
+                    $extra = '';
+                }
+                echo '<option value="' . $c['pk_i_id'] . '"' . $extra . '>' . $deep_string.$c['s_name'] . '</option>' ;
                 if(isset($c['categories']) && is_array($c['categories'])) {
                     CategoryForm::subcategory_select($c['categories'], $category, $default_item, $deep+1);
                 }
