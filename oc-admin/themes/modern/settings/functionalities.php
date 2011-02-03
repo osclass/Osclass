36462d3ccd5c7131eb5694500bf2dc25337d38b7
devconquer <dev.conquer@gmail.com>
2011-02-03 11:20:30 +0100

    Fixing pages

--- oc-admin/themes/modern/settings/functionalities.php
+++ oc-admin/themes/modern/settings/functionalities.php
@@ -23,107 +23,76 @@
 <?php defined('ABS_PATH') or die(__('Invalid OSClass request.')); ?>
 
 <?php
-$dateFormats = array('F j, Y', 'Y/m/d', 'm/d/Y', 'd/m/Y');
-$timeFormats = array('g:i a', 'g:i A', 'H:i');
+    $dateFormats = array('F j, Y', 'Y/m/d', 'm/d/Y', 'd/m/Y');
+    $timeFormats = array('g:i a', 'g:i A', 'H:i');
 ?>
-<script>
-	$(function() {
-		// Here we include specific jQuery, jQuery UI and Datatables functions.
-	});
-</script>
-		<div id="content">
-                    <div id="separator"></div>
 
-			<?php include_once $absolute_path . '/include/backoffice_menu.php'; ?>
+<div id="content">
+    <div id="separator"></div>
 
-		    <div id="right_column">
-				<div id="content_header" class="content_header">
-					<div style="float: left;"><img src="<?php echo  $current_theme; ?>/images/back_office/settings-icon.png" /></div>
-					<div id="content_header_arrow">&raquo; <?php echo __('Functionalities'); ?></div>
-					<div style="clear: both;"></div>
-				</div>
-				
-				<div id="content_separator"></div>
-				<?php osc_showFlashMessages(); ?>
-				<!-- settings form -->
-				<div id="settings_form" style="border: 1px solid #ccc; background: #eee; ">
-					<div style="padding: 20px;">
+    <?php include_once $absolute_path . '/include/backoffice_menu.php'; ?>
 
-						<form action="settings.php" method="post">
-						<input type="hidden" name="action" value="functionalities_post" />
+    <div id="right_column">
+        <div id="content_header" class="content_header">
+            <div style="float: left;"><img src="<?php echo $current_theme; ?>/images/back_office/settings-icon.png" /></div>
+            <div id="content_header_arrow">&raquo; <?php _e('Functionalities'); ?></div>
+            <div style="clear: both;"></div>
+        </div>
 
-                                                <div style="float: left; width: 50%;">
-							<fieldset>
-								<legend><?php echo __('Items'); ?></legend>
-                                                                <?php if(isset($preferences['enabled_recaptcha_items']) && $preferences['enabled_recaptcha_items']): ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" checked="true" name="enabled_recaptcha_items" id="enabled_recaptcha_items"/>
-                                                                <?php else: ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" name="enabled_recaptcha_items" id="enabled_recaptcha_items"/>
-                                                                <?php endif; ?>
-                                                                <label><?php echo __('Enabled recaptcha'); ?></label>
+        <div id="content_separator"></div>
 
-                                                                <br/>
+        <?php osc_showFlashMessages(); ?>
+        
+        <div id="settings_form" style="border: 1px solid #ccc; background: #eee; ">
+            <div style="padding: 20px;">
 
-                                                                <?php if(isset($preferences['enabled_item_validation']) && $preferences['enabled_item_validation']): ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" checked="true" name="enabled_item_validation" id="enabled_item_validation"/>
-                                                                <?php else: ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" name="enabled_item_validation" id="enabled_item_validation"/>
-                                                                <?php endif; ?>
-                                                                <label><?php echo __('Enabled item validation'); ?></label>
+                <form action="settings.php" method="post">
+                    <input type="hidden" name="action" value="functionalities_post" />
 
-                                                                <br/>
+                    <div style="float: left; width: 50%;">
+                        <fieldset>
+                            <legend><?php _e('Items'); ?></legend>
+                            <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" <?php ( osc_enabled_recaptcha_items() ) ? 'checked' : '' ; ?> name="enabled_recaptcha_items" id="enabled_recaptcha_items" />
+                            <label><?php _e('Enabled recaptcha'); ?></label>
+                            <br/>
 
-                                                                <?php if(isset($preferences['reg_user_post']) && $preferences['reg_user_post']): ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" checked="true" name="reg_user_post" id="reg_user_post"/>
-                                                                <?php else: ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" name="reg_user_post" id="reg_user_post"/>
-                                                                <?php endif; ?>
-                                                                <label><?php echo __('Only allow registered users post items'); ?></label>
-                                                        </fieldset>
-						</div>
+                            <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" <?php ( osc_enabled_item_validation() ) ? 'checked' : '' ; ?> name="enabled_item_validation" id="enabled_item_validation" />
+                            <label><?php _e('Enabled item validation'); ?></label>
+                            <br/>
 
-						<div style="float: left; width: 50%;">
-							<fieldset>
-								<legend><?php echo __('Comments'); ?></legend>
-                                                                <?php if(isset($preferences['enabled_comments']) && $preferences['enabled_comments']): ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" checked="true" name="enabled_comments" id="enabled_comments"/>
-                                                                <?php else: ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" name="enabled_comments" id="enabled_comments"/>
-                                                                <?php endif; ?>
-                                                                <label><?php echo __('Comments enabled'); ?></label>
+                            <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" <?php ( osc_reg_user_post() ) ? 'checked' : '' ; ?> name="reg_user_post" id="reg_user_post" />
+                            <label><?php _e('Only allow registered users post items'); ?></label>
+                        </fieldset>
+                     </div>
 
-																<br/>
+                     <div style="float: left; width: 50%;">
+                        <fieldset>
+                            <legend><?php _e('Comments') ; ?></legend>
+                            <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" <?php ( osc_enabled_comments() ) ? 'checked' : '' ; ?> name="enabled_comments" id="enabled_comments" />
+                            <label><?php _e('Comments enabled') ; ?></label>
+                            <br/>
 
-                                                                <?php if(isset($preferences['moderate_comments']) && $preferences['moderate_comments']): ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" checked="true" name="moderate_comments" id="moderate_comments"/>
-                                                                <?php else: ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" name="moderate_comments" id="moderate_comments"/>
-                                                                <?php endif; ?>
-                                                                <label><?php echo __('Moderate comments'); ?></label>
-                                                        </fieldset>
+                            <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" <?php ( osc_moderate_comments() ) ? 'checked' : '' ; ?> name="moderate_comments" id="moderate_comments" />
+                            <label><?php _e('Moderate comments') ; ?></label>
+                        </fieldset>
 
+                        <fieldset>
+                            <legend><?php echo __('Cron System'); ?></legend>
+                            <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" <?php ( osc_auto_cron() ) ? 'checked' : '' ; ?> name="auto_cron" id="auto_cron" />
+                            <label><?php _e('Auto-cron'); ?></label>
+                            <br/>
 
-							<fieldset>
-								<legend><?php echo __('Cron System'); ?></legend>
-                                                                <?php if(isset($preferences['auto_cron']) && $preferences['auto_cron']): ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" checked="true" name="auto_cron" id="auto_cron"/>
-                                                                <?php else: ?>
-                                                                <input style="height: 20px; padding-left: 4px;padding-top: 4px;" type="checkbox" name="auto_cron" id="auto_cron"/>
-                                                                <?php endif; ?>
-                                                                <label><?php echo __('Auto-cron'); ?></label>
+                            <label><?php _e('Some functionalities os OSClass requires a cron system to work. Check this if you don\'t know what a cron-job is or your host is not able to do them. Uncheck if you want to do your cron manually. Refer to the manual to know more about the cron system in OSClass.'); ?></label>
+                        </fieldset>
+                    </div>
+                    <div style="clear: both;"></div>
 
-																<br/>
-                                                                <label><?php echo __('Some functionalities os OSClass requires a cron system to work. Check this if you don\'t know what a cron-job is or your host is not able to do them. Uncheck if you want to do your cron manually. Refer to the manual to know more about the cron system in OSClass.'); ?></label>
-                                                        </fieldset>
+                    <input id="button_save" type="submit" value="<?php _e('Update'); ?>" />
 
-						</div>
+                </form>
 
-						<div style="clear: both;"></div>
-												
-						<input id="button_save" type="submit" value="<?php echo __('Update'); ?>" />
-						
-						</form>
-
-					</div>
-				</div>
-			</div> <!-- end of right column -->
+            </div>
+            
+        </div>
+        
+    </div>
