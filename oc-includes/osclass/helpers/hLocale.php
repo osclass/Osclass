28d7743e471eeb0b87588741f74ead071691b416
devconquer <dev.conquer@gmail.com>
2011-02-04 12:34:19 +0100

    osc_all_enabled_locales fixed

--- oc-includes/osclass/helpers/hLocale.php
+++ oc-includes/osclass/helpers/hLocale.php
@@ -1,7 +1,12 @@
 <?php
 
-    function osc_all_enabled_locales() {
-        $locales = Locale::newInstance()->listAllEnabled(false, true)
+    function osc_all_enabled_locales($indexed_by_pk = false) {
+        return ( Locale::newInstance()->listAllEnabled(false, $indexed_by_pk)) ;
+
+    }
+
+    function osc_all_enabled_locales_for_admin($indexed_by_pk = false) {
+        return ( Locale::newInstance()->listAllEnabled(true, $indexed_by_pk)) ;
     }
 
 ?>
