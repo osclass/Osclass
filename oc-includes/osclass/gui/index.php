a0abf7be3bacf9d315e5209a9d35599242955a56
conejo-osclass <desteban@osclass.org>
2011-02-23 21:36:01 +0100

    index for theme

--- oc-includes/osclass/gui/index.php
+++ oc-includes/osclass/gui/index.php
@@ -1,1 +1,14 @@
-<?php /* So say we all */ ?>
+<?php
+
+function modern_theme_info() {
+	return array(
+		'name' => 'OSClass Modern Theme',
+		'version' => '2.0',
+		'description' => 'This is the OSClass modern theme.',
+		'author_name' => 'OSClass',
+		'author_url' => 'http://www.osclass.org/',
+
+		'locations' => array('header', 'categories', 'footer')
+	);
+}
+?>
