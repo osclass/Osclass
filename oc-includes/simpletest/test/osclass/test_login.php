6f952404a27cab0b64b07f85f23fec075c871e6f
conejo-osclass <desteban@osclass.org>
2011-03-02 18:54:35 +0100

    fix

--- oc-includes/simpletest/test/osclass/test_login.php
+++ oc-includes/simpletest/test/osclass/test_login.php
@@ -11,7 +11,7 @@ class TestOfLogin extends WebTestCase {
     function testLoginPage() {
         // MOD_REWRITE DOESN'T WORK, DE-ACTIVATE IT FIRST
         $this->assertTrue($this->get(osc_user_login_url()));
-        $this->setField('email', 'nodani@gmail.com');
+        $this->setField('email', 'test@osclass.org');
         $this->setField('password', 'password');
         $this->click('Log in');
         // CHECK IF WE ENTERED THE USER'S ACCOUNT
