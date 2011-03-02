6ebfd295d8125955e2246e30351473f6cfb56382
conejo-osclass <desteban@osclass.org>
2011-03-02 18:52:45 +0100

    login test and a few fixed for deprecated messages of simpletest

---
+++ oc-includes/simpletest/test/osclass/test_login.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once('../../autorun.php');
+    require_once('../../web_tester.php');
+    require_once('../../reporter.php');
+
+    require_once '../../../../oc-load.php';
+
+class TestOfLogin extends WebTestCase {
+    
+    function testLoginPage() {
+        // MOD_REWRITE DOESN'T WORK, DE-ACTIVATE IT FIRST
+        $this->assertTrue($this->get(osc_user_login_url()));
+        $this->setField('email', 'nodani@gmail.com');
+        $this->setField('password', 'password');
+        $this->click('Log in');
+        // CHECK IF WE ENTERED THE USER'S ACCOUNT
+        $this->assertText('Items from nodani@gmail.com');
+    }        
+    
+}
+
+?>
