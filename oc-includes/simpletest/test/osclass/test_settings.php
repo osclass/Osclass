28333a91c1c039fb79aa660625ff8c0950faf43d
conejo-osclass <desteban@osclass.org>
2011-03-04 19:13:55 +0100

    settings test

--- oc-includes/simpletest/test/osclass/test_settings.php
+++ oc-includes/simpletest/test/osclass/test_settings.php
@@ -42,8 +42,9 @@ class TestOfSettings extends WebTestCase {
         $this->assertField('auto_cron', false);
         // UPDATE TO ORIGINAL STATE
         $this->setField('auto_cron', $pref['cron']);
-        $this->assertField('auto_cron', $pref['cron']);
         $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=cron');
+        $this->assertField('auto_cron', $pref['cron']);
         unset($pref);
         
         /*****************
@@ -163,6 +164,221 @@ class TestOfSettings extends WebTestCase {
         $this->assertField('akismetKey', $pref['akismet_key']);
         $this->assertField('recaptchaPrivKey', $pref['recaptchaPrivKey']);
         $this->assertField('recaptchaPubKey', $pref['recaptchaPubKey']);
+
+        /**********************
+         *** PERMALINKS TAB ***
+         **********************/
+        $pref = array();
+        $pref['rewrite_enabled'] = osc_rewrite_enabled();
+        $this->get(osc_admin_base_url(true).'?page=settings&action=permalinks');
+        // CHECK IF THE VALUE IS CORRECT WITH THE ONE ON THE DATABASE
+        $this->assertField('rewrite_enabled', $pref['rewrite_enabled']);
+        $this->setField('rewrite_enabled', true);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=permalinks');
+        // CHECK IF THE VALUE IS CORRECT WITH (ON)
+        $this->assertField('rewrite_enabled', '1');
+        $this->setField('rewrite_enabled', false);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=permalinks');
+        // CHECK IF THE VALUE IS CORRECT WITH 0 (OFF)
+        $this->assertField('rewrite_enabled', false);
+        // UPDATE TO ORIGINAL STATE
+        $this->setField('rewrite_enabled', $pref['rewrite_enabled']);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=permalinks');
+        $this->assertField('rewrite_enabled', $pref['rewrite_enabled']);
+        unset($pref);
+
+        /*******************
+         *** CONTACT TAB ***
+         *******************/
+        $pref = array();
+        $pref['contact_attachment'] = osc_contact_attachment();
+        $this->get(osc_admin_base_url(true).'?page=settings&action=contact');
+        // CHECK IF THE VALUE IS CORRECT WITH THE ONE ON THE DATABASE
+        $this->assertField('enabled_attachment', $pref['contact_attachment']);
+        $this->setField('enabled_attachment', true);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=contact');
+        // CHECK IF THE VALUE IS CORRECT WITH (ON)
+        $this->assertField('enabled_attachment', true);
+        $this->setField('enabled_attachment', false);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=contact');
+        // CHECK IF THE VALUE IS CORRECT WITH 0 (OFF)
+        $this->assertField('enabled_attachment', false);
+        // UPDATE TO ORIGINAL STATE
+        $this->setField('enabled_attachment', $pref['contact_attachment']);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=contact');
+        $this->assertField('enabled_attachment', $pref['contact_attachment']);
+        unset($pref);
+
+        /*****************
+         *** USERS TAB ***
+         *****************/
+        $pref = array();
+        $pref['enabled_users'] = osc_users_enabled();
+        $pref['enabled_user_validation'] = osc_user_validation_enabled();
+        $pref['enabled_user_registration'] = osc_user_registration_enabled();
+        $this->get(osc_admin_base_url(true).'?page=settings&action=users');
+        // CHECK IF THE VALUE IS CORRECT WITH THE ONE ON THE DATABASE
+        $this->assertField('enabled_users', $pref['enabled_users']);
+        $this->assertField('enabled_user_validation', $pref['enabled_user_validation']);
+        $this->assertField('enabled_user_registration', $pref['enabled_user_registration']);
+        $this->setField('enabled_users', true);
+        $this->setField('enabled_user_validation', true);
+        $this->setField('enabled_user_registration', true);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=users');
+        // CHECK IF THE VALUE IS CORRECT WITH (ON)
+        $this->assertField('enabled_users', true);
+        $this->assertField('enabled_user_validation', true);
+        $this->assertField('enabled_user_registration', true);
+        $this->setField('enabled_users', false);
+        $this->setField('enabled_user_validation', false);
+        $this->setField('enabled_user_registration', false);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=users');
+        // CHECK IF THE VALUE IS CORRECT WITH 0 (OFF)
+        $this->assertField('enabled_users', false);
+        $this->assertField('enabled_user_validation', false);
+        $this->assertField('enabled_user_registration', false);
+        // UPDATE TO ORIGINAL STATE
+        $this->setField('enabled_users', $pref['enabled_users']);
+        $this->setField('enabled_user_validation', $pref['enabled_user_validation']);
+        $this->setField('enabled_user_registration', $pref['enabled_user_registration']);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=users');
+        $this->assertField('enabled_users', $pref['enabled_users']);
+        $this->assertField('enabled_user_validation', $pref['enabled_user_validation']);
+        $this->assertField('enabled_user_registration', $pref['enabled_user_registration']);
+        unset($pref);
+
+        /********************
+         *** COMMENTS TAB ***
+         ********************/
+        $pref = array();
+        $pref['enabled_comments'] = osc_comments_enabled();
+        $pref['moderate_comments'] = osc_moderate_comments();
+        $pref['notify_new_comment'] = osc_notify_new_comment();
+        $this->get(osc_admin_base_url(true).'?page=settings&action=comments');
+        // CHECK IF THE VALUE IS CORRECT WITH THE ONE ON THE DATABASE
+        $this->assertField('enabled_comments', $pref['enabled_comments']);
+        $this->assertField('moderate_comments', $pref['moderate_comments']);
+        $this->assertField('notify_new_comment', $pref['notify_new_comment']);
+        $this->setField('enabled_comments', true);
+        $this->setField('moderate_comments', true);
+        $this->setField('notify_new_comment', true);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=comments');
+        // CHECK IF THE VALUE IS CORRECT WITH (ON)
+        $this->assertField('enabled_comments', true);
+        $this->assertField('moderate_comments', true);
+        $this->assertField('notify_new_comment', true);
+        $this->setField('enabled_comments', false);
+        $this->setField('moderate_comments', false);
+        $this->setField('notify_new_comment', false);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=comments');
+        // CHECK IF THE VALUE IS CORRECT WITH 0 (OFF)
+        $this->assertField('enabled_comments', false);
+        $this->assertField('moderate_comments', false);
+        $this->assertField('notify_new_comment', false);
+        // UPDATE TO ORIGINAL STATE
+        $this->setField('enabled_comments', $pref['enabled_comments']);
+        $this->setField('moderate_comments', $pref['moderate_comments']);
+        $this->setField('notify_new_comment', $pref['notify_new_comment']);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=comments');
+        $this->assertField('enabled_comments', $pref['enabled_comments']);
+        $this->assertField('moderate_comments', $pref['moderate_comments']);
+        $this->assertField('notify_new_comment', $pref['notify_new_comment']);
+        unset($pref);
+
+        /*****************
+         *** ITEMS TAB ***
+         *****************/
+        $pref = array();
+        $pref['enabled_recaptcha_items'] = osc_recaptcha_items_enabled();
+        $pref['enabled_item_validation'] = osc_item_validation_enabled();
+        $pref['reg_user_post'] = osc_reg_user_post();
+        $pref['notify_new_item'] = osc_notify_new_item();
+        $pref['notify_contact_item'] = osc_notify_contact_item();
+        $pref['notify_contact_friends'] = osc_notify_contact_friends();
+        $pref['enableField#f_price@items'] = osc_price_enabled_at_items();
+        $pref['enableField#images@items'] = osc_images_enabled_at_items();
+        $this->get(osc_admin_base_url(true).'?page=settings&action=items');
+        // CHECK IF THE VALUE IS CORRECT WITH THE ONE ON THE DATABASE
+        $this->assertField('enabled_recaptcha_items', $pref['enabled_recaptcha_items']);
+        $this->assertField('enabled_item_validation', $pref['enabled_item_validation']);
+        $this->assertField('reg_user_post', $pref['reg_user_post']);
+        $this->assertField('notify_new_item', $pref['notify_new_item']);
+        $this->assertField('notify_contact_item', $pref['notify_contact_item']);
+        $this->assertField('notify_contact_friends', $pref['notify_contact_friends']);
+        $this->assertField('enableField#f_price@items', $pref['enableField#f_price@items']);
+        $this->assertField('enableField#images@items', $pref['enableField#images@items']);
+        $this->setField('enabled_recaptcha_items', true);
+        $this->setField('enabled_item_validation', true);
+        $this->setField('reg_user_post', true);
+        $this->setField('notify_new_item', true);
+        $this->setField('notify_contact_item', true);
+        $this->setField('notify_contact_friends', true);
+        $this->setField('enableField#f_price@items', true);
+        $this->setField('enableField#images@items', true);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=items');
+        // CHECK IF THE VALUE IS CORRECT WITH (ON)
+        $this->assertField('enabled_recaptcha_items', true);
+        $this->assertField('enabled_item_validation', true);
+        $this->assertField('reg_user_post', true);
+        $this->assertField('notify_new_item', true);
+        $this->assertField('notify_contact_item', true);
+        $this->assertField('notify_contact_friends', true);
+        $this->assertField('enableField#f_price@items', true);
+        $this->assertField('enableField#images@items', true);
+        $this->setField('enabled_recaptcha_items', false);
+        $this->setField('enabled_item_validation', false);
+        $this->setField('reg_user_post', false);
+        $this->setField('notify_new_item', false);
+        $this->setField('notify_contact_item', false);
+        $this->setField('notify_contact_friends', false);
+        $this->setField('enableField#f_price@items', false);
+        $this->setField('enableField#images@items', false);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=items');
+        // CHECK IF THE VALUE IS CORRECT WITH 0 (OFF)
+        $this->assertField('enabled_recaptcha_items', false);
+        $this->assertField('enabled_item_validation', false);
+        $this->assertField('reg_user_post', false);
+        $this->assertField('notify_new_item', false);
+        $this->assertField('notify_contact_item', false);
+        $this->assertField('notify_contact_friends', false);
+        $this->assertField('enableField#f_price@items', false);
+        $this->assertField('enableField#images@items', false);
+        // UPDATE TO ORIGINAL STATE
+        $this->setField('enabled_recaptcha_items', $pref['enabled_recaptcha_items']);
+        $this->setField('enabled_item_validation', $pref['enabled_item_validation']);
+        $this->setField('reg_user_post', $pref['reg_user_post']);
+        $this->setField('notify_new_item', $pref['notify_new_item']);
+        $this->setField('notify_contact_item', $pref['notify_contact_item']);
+        $this->setField('notify_contact_friends', $pref['notify_contact_friends']);
+        $this->setField('enableField#f_price@items', $pref['enableField#f_price@items']);
+        $this->setField('enableField#images@items', $pref['enableField#images@items']);
+        $this->click('Update');
+        $this->get(osc_admin_base_url(true).'?page=settings&action=items');
+        $this->assertField('enabled_recaptcha_items', $pref['enabled_recaptcha_items']);
+        $this->assertField('enabled_item_validation', $pref['enabled_item_validation']);
+        $this->assertField('reg_user_post', $pref['reg_user_post']);
+        $this->assertField('notify_new_item', $pref['notify_new_item']);
+        $this->assertField('notify_contact_item', $pref['notify_contact_item']);
+        $this->assertField('notify_contact_friends', $pref['notify_contact_friends']);
+        $this->assertField('enableField#f_price@items', $pref['enableField#f_price@items']);
+        $this->assertField('enableField#images@items', $pref['enableField#images@items']);
+        unset($pref);
+
+
 
         
         // We did our tests, lets get back to normal
