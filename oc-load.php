1b56e3cf58bacd78811c0ebbc7ab8ec42387584a
Juan Ramon <juanramon.diaz@gmail.com>
2011-03-15 18:57:04 +0100

    updated OSCLASS_VERSION define

--- oc-load.php
+++ oc-load.php
@@ -19,7 +19,7 @@
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-define('OSCLASS_VERSION', '2.0 RC4') ;
+define('OSCLASS_VERSION', '2.0 RC5') ;
 
 if( !defined('ABS_PATH') ) {
     define( 'ABS_PATH', dirname(__FILE__) . '/' );
