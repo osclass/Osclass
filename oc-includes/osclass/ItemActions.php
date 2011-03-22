a56711ef616fb72a74199ef7a191943e1faa05e1
Juan Ramon <juanramon.diaz@gmail.com>
2011-03-22 12:35:28 +0100

    fixing cannott add items

--- oc-includes/osclass/ItemActions.php
+++ oc-includes/osclass/ItemActions.php
@@ -58,12 +58,11 @@ Class ItemActions
         }
 
         // Validate
-        if ( !preg_match("/([a-z][^a-z]*){3}/i", current($aItem['title']) ) ||
-            !preg_match("/([a-z][^a-z]*){8}/i", current($aItem['description']) ) ||
-            !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/",$contactEmail) ||
+        if ( strlen( reset($aItem['title']) ) == 0 ||
+            strlen( reset($aItem['description']) ) == 0 ||
+            !preg_match("/^.*?@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/",$contactEmail) ||
             !preg_match("/^[0-9]+$/", $aItem['catId'] )) {
             osc_add_flash_message( _m('Some fields were too short. Try again!') );
-            //osc_add_flash_message( _m('You need to insert your name and email to be able to publish a new item'));
             $success = false;
         } else {
             $this->manager->insert(array(
