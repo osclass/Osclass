8de4e695af856215b1bd20be940783eb13d60f14
Carlos Garcia de Marina Vilar <carlos@osclass.org>
2011-04-04 10:08:35 +0200

    test appearance + test widgets

--- oc-includes/simpletest/test/osclass/test_admin_appearance.php
+++ oc-includes/simpletest/test/osclass/test_admin_appearance.php
@@ -101,21 +101,50 @@ class TestOfAdminTools extends WebTestCase {
         }
     }
 
+    function testDeleteTheme()
+    {
+        echo "<div style='background-color: green; color: white;'><h2>testDeleteTheme</h2></div>";
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>testDeleteTheme - LOGIN </div>";
+        $this->loginCorrect();
+        flush();
+        
+        $this->selenium->open( osc_admin_base_url(true) );
+        $this->selenium->click("link=Appearance");
+        $this->selenium->click("link=» Manage themes");
+        $this->selenium->waitForPageToLoad("10000");
+
+        if($this->selenium->isTextPresent("chmod a+w ") ){
+            $this->assertTrue(FALSE, "You need give permissions to the folder");
+        } else {
+
+            $this->selenium->click("link=Activate");
+            $this->selenium->waitForPageToLoad("30000");
+
+            $text_element = $this->selenium->getText("xpath=//div[@id='current_theme_info']" );
+            if(preg_match('/Modern Theme/', $text_element) ) {
+                $this->assertTrue(TRUE);
+            } else {
+                $this->assertTrue(FALSE, "Can't activate the theme");
+            }
+        }
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>testDeleteTheme - <h2>YOU NEED TO DELETE THE PLUGIN DIRECTORY MANUALY ( /oc-content/themes/newcorp/)</h2></div>";
+    }
+
     function testWidgets()
     {
         echo "<div style='background-color: green; color: white;'><h2>testWidgets</h2></div>";
         echo "<div style='background-color: green; color: white;padding-left:15px;'>testWidgets - LOGIN </div>";
         $this->loginCorrect() ;
         flush();
-        echo "<div style='background-color: green; color: white;padding-left:15px;'>testWidgets - WIDGETS</div>";
-        $this->widgets() ;
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>testWidgets - widgetsHeader</div>";
+        $this->widgetsHeader() ;
         flush();
-    }
-
-    function testDeleteTheme()
-    {
-        echo "<div style='background-color: green; color: white;'><h2>testDeleteTheme</h2></div>";
-        echo "<div style='background-color: green; color: white;padding-left:15px;'>testDeleteTheme - YOU NEED TO DELETE THE PLUGIN DIRECTORY MANUALY ( ../themes/newcorp/)</div>";
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>testWidgets - widgetsCategories</div>";
+        $this->widgetsCategories() ;
+        flush();
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>testWidgets - widgetsFooter</div>";
+        $this->widgetsFooter() ;
+        flush();
     }
 
     /*      PRIVATE FUNCTIONS       */
@@ -142,22 +171,88 @@ class TestOfAdminTools extends WebTestCase {
         }
     }
 
-//    private function widgets()
-//    {
-//        $this->selenium->open( osc_admin_base_url(true) );
-//        $this->selenium->click("link=Appearance");
-//        $this->selenium->click("link=» Manage themes");
-//        $this->selenium->waitForPageToLoad("10000");
-//
-//        // add header widget
-//        $this->selenium->click("xpath=//div[@id='settings_form']/div/div[1]/div/a");
-//
-//        // add categories widget
-//        $this->selenium->click("xpath=//div[@id='settings_form']/div/div[1]/div/a");
-//
-//        // add footer widget
-//        $this->selenium->click("xpath=//div[@id='settings_form']/div/div[1]/div/a");
-//    }
+    private function widgetsHeader()
+    {
+        $this->selenium->open( osc_admin_base_url(true) );
+        $this->selenium->click("link=Appearance");
+        $this->selenium->click("link=» Add or remove widgets");
+        $this->selenium->waitForPageToLoad("10000");
 
+        // add header widget
+        $this->selenium->click("xpath=//div[@id='settings_form']/div/div[1]/div/a");
+        $this->selenium->waitForPageToLoad("10000");
+
+        $this->selenium->type("description", "header1");
+        
+        $this->selenium->selectFrame("index=0");
+        $this->selenium->type("xpath=//html/body[@id='tinymce']", "New Widget Header");
+        $this->selenium->selectFrame("relative=top");
+
+        $this->selenium->click("//input[@type='submit']");
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->assertTrue($this->selenium->isTextPresent("header1"), "Can't add widget header. ERROR");
+
+        //remove widget
+        $this->selenium->click("link=Delete");
+        $this->selenium->waitForPageToLoad("30000");
+        $this->assertTrue( ! $this->selenium->isTextPresent("header1"), "Can't delete widget header. ERROR");
+    }
+
+    private function widgetsCategories()
+    {
+        $this->selenium->open( osc_admin_base_url(true) ) ;
+        $this->selenium->click("link=Appearance") ;
+        $this->selenium->click("link=» Add or remove widgets") ;
+        $this->selenium->waitForPageToLoad("10000") ;
+
+        // add categories widget
+        $this->selenium->click("xpath=//div[@id='settings_form']/div/div[2]/div/a") ;
+        $this->selenium->waitForPageToLoad("10000") ;
+
+        $this->selenium->type("description", "categories1") ;
+
+        $this->selenium->selectFrame("index=0");
+        $this->selenium->type("xpath=//html/body[@id='tinymce']", "New Widget Category") ;
+        $this->selenium->selectFrame("relative=top") ;
+
+        $this->selenium->click("//input[@type='submit']") ;
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->assertTrue($this->selenium->isTextPresent("categories1"), "Can't add widget categories. ERROR");
+
+        //remove widget
+        $this->selenium->click("link=Delete");
+        $this->selenium->waitForPageToLoad("30000");
+        $this->assertTrue( ! $this->selenium->isTextPresent("categories1"), "Can't delete widget categories. ERROR");
+    }
+
+    private function widgetsFooter()
+    {
+        $this->selenium->open( osc_admin_base_url(true) );
+        $this->selenium->click("link=Appearance");
+        $this->selenium->click("link=» Add or remove widgets");
+        $this->selenium->waitForPageToLoad("10000");
+
+        // add categories widget
+        $this->selenium->click("xpath=//div[@id='settings_form']/div/div[3]/div/a");
+        $this->selenium->waitForPageToLoad("10000") ;
+
+        $this->selenium->type("description", "footer1") ;
+
+        $this->selenium->selectFrame("index=0");
+        $this->selenium->type("xpath=//html/body[@id='tinymce']", "New Widget Footer") ;
+        $this->selenium->selectFrame("relative=top") ;
+
+        $this->selenium->click("//input[@type='submit']") ;
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->assertTrue($this->selenium->isTextPresent("footer1"), "Can't add widget footer. ERROR");
+
+        //remove widget
+        $this->selenium->click("link=Delete");
+        $this->selenium->waitForPageToLoad("30000");
+        $this->assertTrue( ! $this->selenium->isTextPresent("footer1"), "Can't delete widget footer. ERROR");
+    }
 }
 ?>
