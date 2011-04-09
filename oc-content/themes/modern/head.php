bcd1d9e71d4389a99ec1d0748abf81ec0d94090b
mpiasta-ca <github@mpiasta.ca>
2011-04-09 05:40:17 -0400

    Add indents so when you view-source in browser elements are aligned.

--- oc-content/themes/modern/head.php
+++ oc-content/themes/modern/head.php
@@ -20,23 +20,23 @@
      */
 ?>
 
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-<meta name="generator" content="OSClass <?php echo OSCLASS_VERSION ; ?>" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="generator" content="OSClass <?php echo OSCLASS_VERSION ; ?>" />
 
-<title><?php echo meta_title() ; ?></title>
-<meta name="title" content="<?php echo meta_title() ; ?>" />
-<meta name="description" content="<?php echo meta_description() ; ?>" />
-<meta http-equiv="Cache-Control" content="no-cache" />
-<meta http-equiv="Expires" content="Fri, Jan 01 1970 00:00:00 GMT" />
+        <title><?php echo meta_title() ; ?></title>
+        <meta name="title" content="<?php echo meta_title() ; ?>" />
+        <meta name="description" content="<?php echo meta_description() ; ?>" />
+        <meta http-equiv="Cache-Control" content="no-cache" />
+        <meta http-equiv="Expires" content="Fri, Jan 01 1970 00:00:00 GMT" />
 
-<link href="<?php echo osc_current_web_theme_styles_url('style.css') ; ?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo osc_current_web_theme_styles_url('jquery-ui.css') ; ?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo osc_current_web_theme_styles_url('tabs.css') ; ?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo osc_current_web_theme_styles_url('style.css') ; ?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo osc_current_web_theme_styles_url('jquery-ui.css') ; ?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo osc_current_web_theme_styles_url('tabs.css') ; ?>" rel="stylesheet" type="text/css" />
 
-<script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery.js') ; ?>"></script>
-<script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery-ui.js') ; ?>"></script>
-<script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery-extends.js') ; ?>"></script>
-<script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('global.js') ; ?>"></script>
-<script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('tabber-minimized.js') ; ?>"></script>
+        <script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery.js') ; ?>"></script>
+        <script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery-ui.js') ; ?>"></script>
+        <script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery-extends.js') ; ?>"></script>
+        <script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('global.js') ; ?>"></script>
+        <script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('tabber-minimized.js') ; ?>"></script>
 
 <?php osc_run_hook('header') ; ?>
