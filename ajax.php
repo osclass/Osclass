8444ebe8df66601646115de3f22dff1cc9da634a
Carlos Garcia de Marina Vilar <carlos@osclass.org>
2011-04-12 18:36:57 +0200

    fixed alerts

--- ajax.php
+++ ajax.php
@@ -54,8 +54,13 @@
                     $email = Params::getParam("email");
                     $userid = Params::getParam("userid");
                     if($alert!='' && $email!='') {
+                        if(preg_match("/^[_a-z0-9-+]+(\.[_a-z0-9-+]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/",$email)) {
                             Alerts::newInstance()->createAlert($userid, $email, $alert);
-                        echo "1";
+                            echo "1";
+                        }else{
+                            echo '0';
+                        }
+                        
                         return true;
                     }
                     echo '0';
