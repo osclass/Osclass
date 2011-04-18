75d97f3be2213c906717be1a32d6688709f901a8
conejo-osclass <desteban@osclass.org>
2011-04-18 17:42:57 +0200

    fixed, we were loading a library which we didnt use

--- oc-admin/themes/modern/categories/index.php
+++ oc-admin/themes/modern/categories/index.php
@@ -76,7 +76,6 @@
                 return false;
             }
         </script>
-        <script type="text/javascript" src="<?php echo osc_current_admin_theme_js_url('datatables.post_init.js') ; ?>"></script>
 		<div id="content">
 			<div id="separator"></div>	
 			<?php osc_current_admin_theme_path ( 'include/backoffice_menu.php' ) ; ?>
