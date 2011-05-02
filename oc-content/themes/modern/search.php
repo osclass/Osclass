38839131eacc068e656a525aed09951215e8dc8c
conejo-osclass <conejo@theninjabunny.com>
2011-05-02 17:32:38 +0200

    search fix on links

--- oc-content/themes/modern/search.php
+++ oc-content/themes/modern/search.php
@@ -125,7 +125,7 @@
                             </fieldset>
 
                             <?php
-                                if(osc_search_category() != '') {
+                                if(osc_search_category_id()) {
                                     osc_run_hook('search_form', osc_search_category_id()) ;
                                 } else {
                                     osc_run_hook('search_form') ;
