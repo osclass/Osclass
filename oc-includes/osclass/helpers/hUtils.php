77705eda2acbca21a503937155089d4afcf01799
Carlos <garciademarina@gmail.com>
2011-05-31 17:56:31 +0200

    clean comments

--- oc-includes/osclass/helpers/hUtils.php
+++ oc-includes/osclass/helpers/hUtils.php
@@ -72,9 +72,6 @@
                             }
                         }
                     }
-//                    echo "<pre>";
-//                    print_r($item);
-//                    echo "</pre>";
                 }
             }
         }
