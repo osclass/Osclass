a1a42ea4f482674bf0f4b562bbea9d23d1c213c5
Carlos <garciademarina@gmail.com>
2011-06-09 17:09:11 +0200

    removed contact seller link, if item is expired

--- oc-content/themes/modern/item.php
+++ oc-content/themes/modern/item.php
@@ -79,7 +79,11 @@
                         <p><?php echo osc_item_description() ; ?></p>
                         <?php osc_run_hook('item_detail', osc_item() ) ; ?>
                         <p class="contact_button">
+                            <?php if( !osc_item_is_expired () ) { ?>
+                            <?php     if(osc_reg_user_can_contact() && osc_is_web_user_logged_in() || !osc_reg_user_can_contact() ) { ?>
                             <strong><a href="#contact"><?php _e('Contact seller', 'modern') ; ?></a></strong>
+                            <?php     } ?>
+                            <?php } ?>
                             <strong class="share"><a href="<?php echo osc_item_send_friend_url() ; ?>" rel="nofollow"><?php _e('Share', 'modern') ; ?></a></strong>
                         </p>
                         <?php osc_run_hook('location') ; ?>
