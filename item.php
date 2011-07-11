622b63c932a1c2d84f81d3b7337cf3909b5fd285
conejoninja <conejo@theninjabunny.com>
2011-07-11 17:13:46 +0200

    fiix

--- item.php
+++ item.php
@@ -250,7 +250,7 @@
                         
                         $success = $mItems->edit();
 
-                        osc_run_hook('edited_item', $mItems->findByPrimaryKey($id));
+                        osc_run_hook('edited_item', Item::newInstance()->findByPrimaryKey($id));
                         
                         if($success==1){
                             osc_add_flash_ok_message( _m('Great! We\'ve just updated your item')) ;
