372f1e5034e1b760de244ca1296ff51b289c5ef5
Juan Ramon <juanramon.diaz@gmail.com>
2011-07-18 10:43:55 +0200

    added plural forms to translations

--- oc-includes/php-gettext/gettext.php
+++ oc-includes/php-gettext/gettext.php
@@ -318,7 +318,7 @@ class gettext_reader {
       } else {
         $header = $this->get_translation_string(0);
       }
-      if (eregi("plural-forms: ([^\n]*)\n", $header, $regs))
+      if (preg_match("|plural-forms: ([^\n]*)\n|i", $header, $regs))
         $expr = $regs[1];
       else
         $expr = "nplurals=2; plural=n == 1 ? 0 : 1;";
