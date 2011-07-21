c8cf4ac9d7bb844f206c346e145af02817276c8a
Carlos <garciademarina@gmail.com>
2011-07-21 17:30:50 +0200

    test custom fields done

--- oc-includes/simpletest/test/osclass/test_admin_customfields.php
+++ oc-includes/simpletest/test/osclass/test_admin_customfields.php
@@ -88,16 +88,16 @@ class TestOfAdminCustomFields extends WebTestCase {
 
     }
 
-//    function testDeleteCustomFields()
-//    {
-//        echo "<div style='background-color: green; color: white;'><h2>TestOfAdminCustomFields</h2></div>";
-//        echo "<div style='background-color: green; color: white;padding-left:15px;'>TestOfAdminCustomFields - LOGIN </div>";
-//        $this->loginCorrect() ;
-//        flush();
-//        echo "<div style='background-color: green; color: white;padding-left:15px;'>TestOfAdminCustomFields - Delete all custom fields</div>";
-//        $this->deleteAllFields();
-//        flush();
-//    }
+    function testDeleteCustomFields()
+    {
+        echo "<div style='background-color: green; color: white;'><h2>TestOfAdminCustomFields</h2></div>";
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>TestOfAdminCustomFields - LOGIN </div>";
+        $this->loginCorrect() ;
+        flush();
+        echo "<div style='background-color: green; color: white;padding-left:15px;'>TestOfAdminCustomFields - Delete all custom fields</div>";
+        $this->deleteAllFields();
+        flush();
+    }
 
     /*      PRIVATE FUNCTIONS       */
     private function loginCorrect()
@@ -133,6 +133,10 @@ class TestOfAdminCustomFields extends WebTestCase {
         $this->selenium->click("id=button_add");
         $this->selenium->type("field_name", "extra_field_1");
         $this->selenium->select("field_type", "TEXT");
+
+        $this->selenium->click("xpath=//span[text()='Advanced options']");
+        $this->selenium->type('field_slug','extra_field_1');
+
         $this->selenium->click("id=button_save");
         $this->selenium->waitForPageToLoad("10000");
         $this->assertTrue($this->selenium->isTextPresent("New custom field added"), "Can't add new custom field. ERROR");
@@ -142,6 +146,10 @@ class TestOfAdminCustomFields extends WebTestCase {
         $this->selenium->click("id=button_add");
         $this->selenium->type("field_name", "extra_field_2");
         $this->selenium->select("field_type", "TEXTAREA");
+
+        $this->selenium->click("xpath=//span[text()='Advanced options']");
+        $this->selenium->type('field_slug','extra_field_2');
+
         $this->selenium->click("id=button_save");
         $this->selenium->waitForPageToLoad("10000");
         $this->assertTrue($this->selenium->isTextPresent("New custom field added"), "Can't add new custom field. ERROR");
@@ -158,7 +166,6 @@ class TestOfAdminCustomFields extends WebTestCase {
 
         // edit categories,
         $this->selenium->click("link=Edit");
-        $this->selenium->waitForPageToLoad("10000");
 
         // modificar s_name & type
         $this->selenium->type("xpath=//input[@id='s_name']", "NEW FIELD");
@@ -186,9 +193,9 @@ class TestOfAdminCustomFields extends WebTestCase {
 
         // edit categories,
         $this->selenium->click("link=Edit"); // first Edit link
-        sleep(1);
+        usleep(250000);
         $this->assertTrue($this->selenium->isElementPresent("xpath=//form[@id='field_form']"), "Form is not showed. ERROR");
-        sleep(1);
+        usleep(250000);
         $this->selenium->click("xpath=//div[@id='TableFields']/ul/li[last()]/div/div[2]/a[1]") ;
         sleep(2);
         $var = (int)$this->selenium->getXpathCount("//form[@id='field_form']");
@@ -206,6 +213,10 @@ class TestOfAdminCustomFields extends WebTestCase {
         $this->selenium->type("field_name", "sameField");
         $this->selenium->select("field_type", "TEXT");
         $this->selenium->click("id=field_required");
+
+        $this->selenium->click("xpath=//span[text()='Advanced options']");
+        $this->selenium->type('field_slug','sameField');
+
         $this->selenium->click("id=button_save");
         $this->selenium->waitForPageToLoad("10000");
         $this->assertTrue($this->selenium->isTextPresent("New custom field added"), "Can't add new custom field. ERROR");
@@ -222,11 +233,14 @@ class TestOfAdminCustomFields extends WebTestCase {
     private function customOnFrontEnd()
     {
         $uSettings = new utilSettings();
-        $bool_reg_user_post = $uSettings->set_reg_user_post(0);
+        $bool_reg_user_post  = $uSettings->set_reg_user_post(0);
+        $bool_moderate_items = $uSettings->set_moderate_items(-1);
         // check if custom fields appears at website
         $this->selenium->open( osc_base_url(true) );
         $this->selenium->click("link=Publish your ad for free");
-        $this->selenium->select("id=catId", "label=regexp:\\s+Animals");
+        $this->selenium->waitForPageToLoad("10000");
+        $this->selenium->select("catId", "label=regexp:\\s*Animals");
+        usleep(500000);
         $this->selenium->type("id=title[en_US]", "foo title");
         $this->selenium->type("id=description[en_US]","description foo title");
         $this->selenium->select("countryId", "label=Spain");
@@ -235,18 +249,34 @@ class TestOfAdminCustomFields extends WebTestCase {
         $this->selenium->type("cityArea", "my area");
         $this->selenium->type("address", "my address");
 
+        $this->selenium->type('id=contactName' , 'foobar');
+        $this->selenium->type('id=contactEmail', 'foobar@mail.com');
+
         $this->assertTrue($this->selenium->isTextPresent("extra_field_2"), "Custom fields aren't show at frontend (item_post). ERROR");
         $this->assertTrue($this->selenium->isTextPresent("sameField")    , "Custom fields aren't show at frontend (item_post). ERROR");
         $this->assertTrue($this->selenium->isTextPresent("NEW FIELD")    , "Custom fields aren't show at frontend (item_post). ERROR");
 
-        $this->selenium->type("sameField"    , "custom1");
-        $this->selenium->type("NEW FIELD"    , "custom2");
-        $this->selenium->type("extra_field_2", "custom3");
+        $this->selenium->type("id=meta_extra_field_1"  , "custom2");
+        $this->selenium->type("id=meta_extra_field_2"  , "custom3");
 
+        $this->selenium->click("//button[text()='Publish']");
+        $this->selenium->waitForPageToLoad("30000");
+        $this->assertTrue($this->selenium->isTextPresent("sameField field is required.","(no logged) can add items with customs fields required. ERROR") );
+
+        $this->selenium->type("id=meta_samefield"      , "custom1");
+        $this->selenium->type("id=meta_extra_field_1"  , "custom2");
+        $this->selenium->type("id=meta_extra_field_2"  , "custom3");
+
+        $this->selenium->click("//button[text()='Publish']");
+        $this->selenium->waitForPageToLoad("30000");
         
-        $this->assertTrue($this->selenium->isTextPresent("extra_field_2"), "Custom fields aren't show at frontend (item_post). ERROR");
+        $this->assertTrue($this->selenium->isTextPresent("Your item has been published","(no logged) error inserting ad with customs fields. ERROR") );
         // volver a dejar reg_user_post flag en su estado original
-        $bool_reg_user_post = $uSettings->set_reg_user_post($bool_reg_user_post);
+        $bool_reg_user_post  = $uSettings->set_reg_user_post($bool_reg_user_post);
+        $bool_moderate_items = $uSettings->set_moderate_items($bool_moderate_items);
+
+        // remove item
+        Item::newInstance()->delete( array('s_contact_email' => 'foobar@mail.com') ) ;
     }
 
     private function customOnAdminPanel()
@@ -256,12 +286,39 @@ class TestOfAdminCustomFields extends WebTestCase {
         $this->selenium->click("link=Items");
         $this->selenium->click("link=» Add new item");
         $this->selenium->waitForPageToLoad("10000");
-        sleep(5);
-        $this->selenium->select("id=catId", "label=regexp:\\s+Animals");
+        
+        $this->selenium->select("catId", "label=regexp:\\s*Animals");
         usleep(500000);
-        $this->assertTrue($this->selenium->isTextPresent("extra_field_2"), "Custom fields aren't show at admin panel (item_post). ERROR");
-        $this->assertTrue($this->selenium->isTextPresent("NEW FIELD"), "Custom fields aren't show at admin panel (item_post). ERROR");
-        $this->assertTrue($this->selenium->isTextPresent("sameField"), "Custom fields aren't show at admin panel (item_post). ERROR");
+        $this->selenium->type("id=title[en_US]", "foo title");
+        $this->selenium->type("id=description[en_US]","description foo title");
+        $this->selenium->select("countryId", "label=Spain");
+        $this->selenium->select("regionId", "label=Albacete");
+        $this->selenium->select("cityId", "label=Albacete");
+        $this->selenium->type("cityArea", "my area");
+        $this->selenium->type("address", "my address");
+
+        $this->selenium->type('id=contactName' , 'foobar');
+        $this->selenium->type('id=contactEmail', 'foobar@mail.com');
+
+        $this->assertTrue($this->selenium->isTextPresent("extra_field_2"), "Custom fields aren't show at frontend (item_post). ERROR");
+        $this->assertTrue($this->selenium->isTextPresent("sameField")    , "Custom fields aren't show at frontend (item_post). ERROR");
+        $this->assertTrue($this->selenium->isTextPresent("NEW FIELD")    , "Custom fields aren't show at frontend (item_post). ERROR");
+
+        $this->selenium->type("id=meta_extra_field_1"  , "custom2");
+        $this->selenium->type("id=meta_extra_field_2"  , "custom3");
+
+        $this->selenium->click("//button[text()='Add item']");
+        $this->selenium->waitForPageToLoad("30000");
+        $this->assertTrue($this->selenium->isTextPresent("sameField field is required."),"(no logged) can add items with customs fields required. ERROR" );
+
+        $this->selenium->type("id=meta_samefield"      , "custom1");
+        $this->selenium->type("id=meta_extra_field_1"  , "custom2");
+        $this->selenium->type("id=meta_extra_field_2"  , "custom3");
+
+        $this->selenium->click("//button[text()='Add item']");
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->assertTrue($this->selenium->isTextPresent("A new item has been added"),"(no logged) error inserting ad with customs fields. ERROR" );
     }
 
     private function deleteAllFields()
