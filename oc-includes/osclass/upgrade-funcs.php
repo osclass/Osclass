d7f2d06fda04d02f97f6dec2647bb9a1dc0cf012
conejoninja <conejo@theninjabunny.com>
2011-08-30 12:36:34 +0200

    fix

--- oc-includes/osclass/upgrade-funcs.php
+++ oc-includes/osclass/upgrade-funcs.php
@@ -142,6 +142,9 @@
         $conn->osc_dbExec(sprintf("INSERT INTO %st_preference VALUES ('osclass', 'mailserver_pop', '', 'STRING')", DB_TABLE_PREFIX));
         $conn->osc_dbExec(sprintf("INSERT INTO %st_preference VALUES ('osclass', 'use_imagick', '0', 'BOOLEAN')", DB_TABLE_PREFIX));
         $timezone = 'Europe/Belgrade';
+        if(ini_get('date.timezone')!='') {
+            $timezone = init_get('date.timezone');
+        };
         if(date_default_timezone_get()!='') {
             $timezone = date_default_timezone_get();
         };
