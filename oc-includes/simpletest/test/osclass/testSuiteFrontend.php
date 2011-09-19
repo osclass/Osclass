c95611b9b912184a57fe6b526330cd170f4e7bd5
Carlos <garciademarina@gmail.com>
2011-09-19 12:19:15 +0200

    minor tests

--- oc-includes/simpletest/test/osclass/testSuiteFrontend.php
+++ oc-includes/simpletest/test/osclass/testSuiteFrontend.php
@@ -7,11 +7,11 @@ class AllFrontEndTests extends TestSuite {
     function AllFrontEndTests() {
         
         $this->TestSuite('All tests frontend');
-        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-contactForm.php');
-        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-login.php');
-        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-register.php');
+//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-contactForm.php');
+//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-login.php');
+//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-register.php');
         $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-search.php');
-        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-items.php');
+//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-items.php');
 
 
         
