d4d8782efdb3f3e829de96f6fcd24f4d353cc3fe
Carlos <garciademarina@gmail.com>
2011-09-20 17:46:24 +0200

    my config

--- oc-includes/simpletest/test/osclass/config_test.php
+++ oc-includes/simpletest/test/osclass/config_test.php
@@ -1,11 +1,12 @@
 <?php
 
 // selenium config
-$browser = "*googlechrome";
+//$browser = "*googlechrome";
+$browser = "*firefox";
 $speed = '150';
 
 // test config
-$email      = "nodani+tests@gmail.com";
+$email      = "carlos+tests@gmail.com";
 $password   = "12345678";
 // test admin config
 $email_admin    = $email;
