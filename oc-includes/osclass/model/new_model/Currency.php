03e032b01bda6072e4eef320c35bc1e9d32494cb
Carlos <garciademarina@gmail.com>
2011-10-04 16:23:00 +0200

    added Currency model

---
+++ oc-includes/osclass/model/new_model/Currency.php
@@ -0,0 +1,75 @@
+<?php
+
+    /*
+     *      OSCLass – software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2010 OSCLASS
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+
+    /**
+     * Currency DAO
+     */
+    class Currency extends DAO
+    {
+        /**
+         *
+         * @var type 
+         */
+        private static $instance ;
+
+        public static function newInstance()
+        {
+            if( !self::$instance instanceof self ) {
+                self::$instance = new self ;
+            }
+            return self::$instance ;
+        }
+
+        /**
+         * 
+         */
+        function __construct()
+        {
+            parent::__construct();
+            $this->set_table_name('t_currency') ;
+            $this->set_primary_key('pk_c_code') ;
+            $this->set_fields(array('s_name', 's_description', 'b_enabled')) ;
+        }
+        
+        /**
+         * Searches for locale information, given a code language.
+         * If code language not exist return false.
+         * 
+         * @param string $code
+         * @return array 
+         */
+        function findBycode($code)
+        {
+            $this->dao->select();
+            $this->dao->from($this->table_name) ;
+            $this->dao->where('pk_c_code', $code) ;
+            $result = $this->dao->get() ;
+            
+            if( $result->num_rows == 0 ) {
+                return false;
+            } else {
+                return $result->row();
+            }
+        }
+        
+    }
+?>
