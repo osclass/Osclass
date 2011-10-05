d129f6c85ef64fdcb4f6f1c444333b2ebc555b5f
Juan Ramon <juanramon.diaz@gmail.com>
2011-10-05 18:58:33 +0200

    siteInfo class updated to

--- oc-includes/t/config.php
+++ oc-includes/t/config.php
@@ -1,7 +1,9 @@
 <?php
     define('DB_HOST', 'localhost') ;
     define('DB_USER', 'root') ;
-    define('DB_PASSWORD', 'password') ;
-    define('DB_NAME', 'osclass_p') ;
+    define('DB_PASSWORD', '') ;
+    define('DB_NAME', 'osclass') ;
     define('DB_TABLE_PREFIX', 'oc_') ;
+    define('DEBUG_LEVEL', 0) ;
+    define('MULTISITE', 0) ;
 ?>
