711b2fb196ecd07e01e2891a013196327b7a4023
Carlos <garciademarina@gmail.com>
2011-10-06 10:01:40 +0200

    carlos config

--- oc-includes/t/config.php
+++ oc-includes/t/config.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_HOST', 'localhost') ;
     define('DB_USER', 'root') ;
-    define('DB_PASSWORD', 'password') ;
-    define('DB_NAME', 'osclass_p') ;
+    define('DB_PASSWORD', '') ;
+    define('DB_NAME', 'osclass') ;
     define('DB_TABLE_PREFIX', 'oc_') ;
 ?>
