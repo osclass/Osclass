cd641bd4e2c8cdf10842187acd7fb66bd8dabb02
Juan Ramon <juanramon.diaz@gmail.com>
2011-10-20 17:47:40 +0200

    renamed database folder and references in tests

--- oc-includes/t/CronTest.php
+++ oc-includes/t/CronTest.php
@@ -22,10 +22,10 @@
 
     require_once '../osclass/Logger/LogDatabase.php' ;
     require_once '../osclass/helpers/hDatabaseInfo.php' ;
-    require_once '../osclass/classes/data/DBConnectionClass.php' ;
-    require_once '../osclass/classes/data/DBCommandClass.php' ;
-    require_once '../osclass/classes/data/DBRecordsetClass.php' ;
-    require_once '../osclass/classes/data/DAO.php' ;
+    require_once '../osclass/classes/database/DBConnectionClass.php' ;
+    require_once '../osclass/classes/database/DBCommandClass.php' ;
+    require_once '../osclass/classes/database/DBRecordsetClass.php' ;
+    require_once '../osclass/classes/database/DAO.php' ;
 
     require_once '../osclass/model/new_model/Cron.php' ;
 
