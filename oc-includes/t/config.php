001542c9068ce7b73d026fba7cbfe990b45df570
Carlos <garciademarina@gmail.com>
2011-10-25 12:57:34 +0200

    fixed model Page, added test model page

--- oc-includes/t/config.php
+++ oc-includes/t/config.php
@@ -1,8 +1,8 @@
 <?php
     define('DB_HOST', 'localhost') ;
     define('DB_USER', 'root') ;
-    define('DB_PASSWORD', 'password') ;
-    define('DB_NAME', 'osclass_p') ;
-    define('DB_TABLE_PREFIX', 'oc_') ;
+    define('DB_PASSWORD', '') ;
+    define('DB_NAME', 'osclass') ;
+    define('DB_TABLE_PREFIX', 'test_') ;
     define('MULTISITE', 0) ;
 ?>
