e2bf298b577cb3a558ff31e2a5422e21da721256
Carlos <garciademarina@gmail.com>
2011-10-25 15:10:21 +0200

    testing oc-admin

--- oc-includes/simpletest/test/osclass/testSuiteFrontend.php
+++ oc-includes/simpletest/test/osclass/testSuiteFrontend.php
@@ -7,12 +7,12 @@ class AllFrontEndTests extends TestSuite {
     function AllFrontEndTests() {
         
         $this->TestSuite('All tests frontend');
-//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-contactForm.php');
-//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-login.php');
-//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-register.php');
-//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-search.php');
-//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-items.php');
-//        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-page.php');        
+        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-contactForm.php');
+        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-login.php');
+        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-register.php');
+        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-search.php');
+        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-items.php');
+        $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-page.php');        
         $this->addFile(ABS_PATH . 'oc-includes/simpletest/test/osclass/Frontend-users.php');        
     }
     
