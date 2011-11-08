739bcbe7870200d50e090a9579afcfe20e45fc05
Juan Ramon <juanramon.diaz@gmail.com>
2011-11-08 16:21:04 +0100

    deleted model tests from develop

--- oc-includes/t/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost') ;
-    define('DB_USER', 'root') ;
-    define('DB_PASSWORD', '') ;
-    define('DB_NAME', 'osclass') ;
-    define('DB_TABLE_PREFIX', 'test_') ;
-    define('MULTISITE', 0) ;
-?>
