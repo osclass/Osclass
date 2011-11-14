40c5ae3cf2d5ba51c28eb591a61b692d4f606572
Juan Ramon <juanramon.diaz@gmail.com>
2011-11-14 19:27:41 +0100

    Update region and city in t_item_location after editing them in the oc-admin. fixes #16

--- oc-admin/settings.php
+++ oc-admin/settings.php
@@ -226,6 +226,10 @@
                                                                         if($regionId != '') {
                                                                             $mRegions->update(array('s_name' => $newRegion)
                                                                                              ,array('pk_i_id' => $regionId));
+                                                                            ItemLocation::newInstance()->update(
+                                                                                array('s_region'       => $newRegion),
+                                                                                array('fk_i_region_id' => $regionId)
+                                                                            );
                                                                             osc_add_flash_ok_message(sprintf(_m('%s has been edited'),
                                                                                                               $newRegion), 'admin');
                                                                         }
@@ -281,7 +285,10 @@
                                                                     if(!isset($exists['pk_i_id']) || $exists['pk_i_id']==$cityId) {
                                                                         $mCities->update(array('s_name' => $newCity)
                                                                                         ,array('pk_i_id' => $cityId));
-
+                                                                        ItemLocation::newInstance()->update(
+                                                                            array('s_city'       => $newCity),
+                                                                            array('fk_i_city_id' => $cityId)
+                                                                        );
                                                                         osc_add_flash_ok_message(sprintf(_m('%s has been edited'),
                                                                                                          $newCity), 'admin');
                                                                     } else {
