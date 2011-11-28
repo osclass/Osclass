3a3b9b17533b3c525109128e0f16ade41e65fa0a
Juan Ramón <juanramon.diaz@gmail.com>
2011-11-28 15:28:00 +0100

    Bug in decreaseNumItems method in CategoryStats model fixes #33 fixes #11
    The update SQL for decreaseNumItems didn't set well the SET key. It was affecting when you delete and edit an item.

--- oc-includes/osclass/classes/database/DBCommandClass.php
+++ oc-includes/osclass/classes/database/DBCommandClass.php
@@ -1261,14 +1261,18 @@
          * @param mixed $value
          * @return DBCommandClass 
          */
-        function set($key, $value = '')
+        function set($key, $value = '', $escape = true)
         {
             if( !is_array($key) ) {
                 $key = array($key => $value) ;
             }
 
             foreach($key as $k => $v) {
-                $this->aSet[$k] = $this->escape($v) ;
+                if( $escape ) {
+                    $this->aSet[$k] = $this->escape($v) ;
+                } else {
+                    $this->aSet[$k] = $v ;
+                }
             }
 
             return $this ;
