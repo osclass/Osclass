da41830638e1718d5d5ea7912da5058780ce05ed
Juan Ramón <juanramon.diaz@gmail.com>
2011-12-13 09:42:22 +0100

    v2.3.2

--- readme.php
+++ readme.php
@@ -28,7 +28,7 @@
                             <img src="oc-includes/images/osclass-logo.png" alt="OSClass" title="OSClass"/>
                         </a>
                         <br/>
-                        Version 2.3.1
+                        Version 2.3.2
                     </h1>
                 </div>
                 <div id="content">
