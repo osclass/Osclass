a647165c5fa224f00bfffa74b1b2dd5c683321d1
Juan Ramón <juanramon.diaz@gmail.com>
2011-12-14 16:19:05 +0100

    using __() function when editing a comment

--- oc-admin/themes/modern/comments/frm.php
+++ oc-admin/themes/modern/comments/frm.php
@@ -118,11 +118,11 @@
                             </div>
                         </div>
                         <div class="FormElement">
-                            <div class="FormElementName"><?php _e('Status'); ?>: <?php echo ($comment['b_active']?__('ACTIVE'):_('INACTIVE')); ?> ( <a href="<?php echo osc_admin_base_url(true);?>?page=comments&action=status&id=<?php echo $comment['pk_i_id'];?>&value=<?php echo (($comment['b_active']==1)?'INACTIVE':'ACTIVE');?>"><?php echo (($comment['b_active']==1)?__('De-activate'):__('Activate'));?></a> )
+                            <div class="FormElementName"><?php _e('Status'); ?>: <?php echo ( $comment['b_active'] ? __('ACTIVE') : __('INACTIVE') ) ; ?> ( <a href="<?php echo osc_admin_base_url( true ) ; ?>?page=comments&action=status&id=<?php echo $comment['pk_i_id'] ; ?>&value=<?php echo ( ( $comment['b_active'] == 1) ? 'INACTIVE' : 'ACTIVE' ) ; ?>"><?php echo ( ( $comment['b_active'] == 1 ) ? __('De-activate') : __('Activate') ) ; ?></a> )
                             </div>
                         </div>
                         <div class="FormElement">
-                            <div class="FormElementName"><?php _e('Status'); ?>: <?php echo ($comment['b_enabled']?__('ENABLED'):_('DISABLED')); ?> ( <a href="<?php echo osc_admin_base_url(true);?>?page=comments&action=status&id=<?php echo $comment['pk_i_id'];?>&value=<?php echo (($comment['b_enabled']==1)?'DISABLE':'ENABLE');?>"><?php echo (($comment['b_enabled']==1)?__('Disable'):__('Enable'));?></a> )
+                            <div class="FormElementName"><?php _e('Status'); ?>: <?php echo ( $comment['b_enabled'] ? __('ENABLED') : __('DISABLED') ) ; ?> ( <a href="<?php echo osc_admin_base_url( true ) ; ?>?page=comments&action=status&id=<?php echo $comment['pk_i_id'] ; ?>&value=<?php echo ( ( $comment['b_enabled'] == 1) ? 'DISABLE' : 'ENABLE' ) ; ?>"><?php echo ( ( $comment['b_enabled'] == 1 ) ? __('Disable') : __('Enable') ) ; ?></a> )
                             </div>
                         </div>
                         <div class="FormElement">
