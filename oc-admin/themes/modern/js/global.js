7eaff5cbb822a11ed842d4086d9b5d3e4aa68014
Juan Ramón <juanramon.diaz@gmail.com>
2012-02-06 15:57:50 +0100

    fixed sidebar menu in oc-admin

--- oc-admin/themes/modern/js/global.js
+++ oc-admin/themes/modern/js/global.js
@@ -4,7 +4,36 @@ $(document).ready(function() {
         collapsible: true,
         navigation: true,
         autoHeight: false,
-        icons: { 'header': 'ui-icon-plus', 'headerSelected': 'ui-icon-minus' }
+        navigationFilter: function() {
+            var menu_url = this.href.toLowerCase() ;
+            var res_url = '' ;
+            var loc_url = location.href.toLowerCase() ;
+
+            var matches = loc_url.match(/(.*?)\?(.*)/) ;
+
+            if( matches.length != 3 ) {
+                return false ;
+            }
+
+            var params = matches[2] ;
+            var params_res = '' ;
+            var obj = params.split('&') ;
+            for(var i = 0; i < obj.length; i++) {
+                var chunk = obj[i].split('=') ;
+                if( chunk.length > 0 ) {
+                    if( jQuery.inArray(chunk[0], ['page', 'action', 'file']) >= 0 ) {
+                        if( params_res == '' ) {
+                            params_res += obj[i] ;
+                        } else {
+                            params_res += '&' + obj[i] ;
+                        }
+                    }
+                }
+            }
+            res_url = matches[1] + '?' + params_res ;
+            return menu_url === res_url ;
+        },
+        icons: {'header': 'ui-icon-plus', 'headerSelected': 'ui-icon-minus'}
     }) ;
 
     if( jQuery.browser.msie && jQuery.browser.version.substr(0,1) < 7 ) {
