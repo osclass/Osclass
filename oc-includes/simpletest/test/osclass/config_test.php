2e3f1237896c1dc25ce58978e212ee5036118ccf
conejoninja <conejo@theninjabunny.com>
2012-03-12 17:46:23 +0100

    wip

--- oc-includes/simpletest/test/osclass/config_test.php
+++ oc-includes/simpletest/test/osclass/config_test.php
@@ -1,13 +1,11 @@
 <?php
 if(file_exists(dirname(__FILE__)."/custom_config.php")) {
-    print_r("EL FICHERO EXISTE");
     // ALLOW OVERRIDE OF CONFIGURATION
     require dirname(__FILE__)."/custom_config.php";
 } else {
-    print_r("EL FICHERO *NO* EXISTE");
     // selenium config
     //$browser = "*googlechrome";
-    /*$browser = "*firefox";
+    $browser = "*firefox";
     $speed = '150';
 
 
@@ -18,10 +16,10 @@ if(file_exists(dirname(__FILE__)."/custom_config.php")) {
     $db_pass = "";
 
     // test config
-    $email      = "carlos+tests@osclass.org";
+    $email      = "testing@osclass.org";
     $password   = "12345678";
     // test admin config
     $email_admin    = $email;
-    $password_admin = 'password';*/
+    $password_admin = 'password';
 };
 ?>
