0676b46ec1b4a40d472872146460e5d8b2b26312
Juan Ramón <juanramon.diaz@gmail.com>
2012-04-23 13:55:35 +0200

    2.3.6 references to 2.4

--- readme.php
+++ readme.php
@@ -26,7 +26,7 @@
                             <img src="oc-includes/images/osclass-logo.png" alt="OSClass" title="OSClass"/>
                         </a>
                         <br/>
-                        Version 2.3.6
+                        Version 2.4
                     </h1>
                 </div>
                 <div id="content">
