bacdd193d54132d0a41e9c702c3676d26de27139
Juan Ramón <juanramon.diaz@gmail.com>
2012-04-23 13:43:20 +0200

    updated gui folder

--- oc-includes/osclass/gui/item-send-friend.php
+++ oc-includes/osclass/gui/item-send-friend.php
@@ -19,7 +19,6 @@
      * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="<?php echo str_replace('_', '-', osc_current_user_locale()); ?>">
     <head>
@@ -29,39 +28,35 @@
         <script type="text/javascript" src="<?php echo osc_current_web_theme_js_url('jquery.validate.min.js') ; ?>"></script>
     </head>
     <body>
-        <div class="container">
-            <?php osc_current_web_theme_path('header.php') ; ?>
-            <div class="content user_forms">
-                <div id="contact" class="inner">
-                    <h1><?php _e('Send to a friend', 'modern'); ?></h1>
-                    <ul id="error_list"></ul>
-                    <form id="sendfriend" name="sendfriend" action="<?php echo osc_base_url(true); ?>" method="post">
-                        <fieldset>
-                            <input type="hidden" name="action" value="send_friend_post" />
-                            <input type="hidden" name="page" value="item" />
-                            <input type="hidden" name="id" value="<?php echo osc_item_id(); ?>" />
-                            <label><?php _e('Item', 'modern'); ?>: <a href="<?php echo osc_item_url( ); ?>"><?php echo osc_item_title(); ?></a></label><br/>
-                            <?php if(osc_is_web_user_logged_in()) { ?>
-                                <input type="hidden" name="yourName" value="<?php echo osc_logged_user_name(); ?>" />
-                                <input type="hidden" name="yourEmail" value="<?php echo osc_logged_user_email();?>" />
-                            <?php } else { ?>
-                                <label for="yourName"><?php _e('Your name', 'modern'); ?></label> <?php SendFriendForm::your_name(); ?> <br/>
-                                <label for="yourEmail"><?php _e('Your e-mail address', 'modern'); ?></label> <?php SendFriendForm::your_email(); ?> <br/>
-                            <?php }; ?>
-                            <label for="friendName"><?php _e("Your friend's name", 'modern'); ?></label> <?php SendFriendForm::friend_name(); ?> <br/>
-                            <label for="friendEmail"><?php _e("Your friend's e-mail address", 'modern'); ?></label> <?php SendFriendForm::friend_email(); ?> <br/>
-                            <label for="message"><?php _e('Message', 'modern'); ?></label> <?php SendFriendForm::your_message(); ?> <br/>
-                            <?php osc_show_recaptcha(); ?>
-                            <br/>
-                            <button type="submit"><?php _e('Send', 'modern'); ?></button>
-                        </fieldset>
-                    </form>
-                </div>
+        <?php osc_current_web_theme_path('header.php') ; ?>
+        <div class="content user_forms">
+            <div id="contact" class="inner">
+                <h1><?php _e('Send to a friend', 'modern'); ?></h1>
+                <ul id="error_list"></ul>
+                <form id="sendfriend" name="sendfriend" action="<?php echo osc_base_url(true); ?>" method="post">
+                    <fieldset>
+                        <input type="hidden" name="action" value="send_friend_post" />
+                        <input type="hidden" name="page" value="item" />
+                        <input type="hidden" name="id" value="<?php echo osc_item_id(); ?>" />
+                        <label><?php _e('Item', 'modern'); ?>: <a href="<?php echo osc_item_url( ); ?>"><?php echo osc_item_title(); ?></a></label><br/>
+                        <?php if(osc_is_web_user_logged_in()) { ?>
+                            <input type="hidden" name="yourName" value="<?php echo osc_logged_user_name(); ?>" />
+                            <input type="hidden" name="yourEmail" value="<?php echo osc_logged_user_email();?>" />
+                        <?php } else { ?>
+                            <label for="yourName"><?php _e('Your name', 'modern'); ?></label> <?php SendFriendForm::your_name(); ?> <br/>
+                            <label for="yourEmail"><?php _e('Your e-mail address', 'modern'); ?></label> <?php SendFriendForm::your_email(); ?> <br/>
+                        <?php }; ?>
+                        <label for="friendName"><?php _e("Your friend's name", 'modern'); ?></label> <?php SendFriendForm::friend_name(); ?> <br/>
+                        <label for="friendEmail"><?php _e("Your friend's e-mail address", 'modern'); ?></label> <?php SendFriendForm::friend_email(); ?> <br/>
+                        <label for="message"><?php _e('Message', 'modern'); ?></label> <?php SendFriendForm::your_message(); ?> <br/>
+                        <?php osc_show_recaptcha(); ?>
+                        <br/>
+                        <button type="submit"><?php _e('Send', 'modern'); ?></button>
+                    </fieldset>
+                </form>
             </div>
-            <?php SendFriendForm::js_validation(); ?>
-            <?php osc_current_web_theme_path('footer.php') ; ?>
         </div>
-        <?php osc_show_flash_message() ; ?>
-        <?php osc_run_hook('footer'); ?>
+        <?php SendFriendForm::js_validation(); ?>
+        <?php osc_current_web_theme_path('footer.php') ; ?>
     </body>
 </html>
--- oc-includes/osclass/gui/user-items.php
+++ oc-includes/osclass/gui/user-items.php
@@ -19,7 +19,6 @@
      * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="<?php echo str_replace('_', '-', osc_current_user_locale()); ?>">
     <head>
@@ -28,57 +27,53 @@
         <meta name="googlebot" content="noindex, nofollow" />
     </head>
     <body>
-        <div class="container">
-            <?php osc_current_web_theme_path('header.php') ; ?>
-            <div class="content user_account">
-                <h1>
-                    <strong><?php _e('User account manager', 'modern') ; ?></strong>
-                </h1>
-                <div id="sidebar">
-                    <?php echo osc_private_user_menu() ; ?>
-                </div>
-                <div id="main">
-                    <h2><?php _e('Your items', 'modern'); ?> <a href="<?php echo osc_item_post_url() ; ?>">+ <?php _e('Post a new item', 'modern'); ?></a></h2>
-                    <?php if(osc_count_items() == 0) { ?>
-                        <h3><?php _e('You don\'t have any items yet', 'modern'); ?></h3>
-                    <?php } else { ?>
-                        <?php while(osc_has_items()) { ?>
-                                <div class="item" >
-                                        <h3>
-                                            <a href="<?php echo osc_item_url(); ?>"><?php echo osc_item_title(); ?></a>
-                                        </h3>
-                                        <p>
-                                        <?php _e('Publication date', 'modern') ; ?>: <?php echo osc_format_date(osc_item_pub_date()) ; ?><br />
-                                        <?php if( osc_price_enabled_at_items() ) { _e('Price', 'modern') ; ?>: <?php echo osc_format_price(osc_item_price()); } ?>
-                                        </p>
-                                        <p class="options">
-                                            <strong><a href="<?php echo osc_item_edit_url(); ?>"><?php _e('Edit', 'modern'); ?></a></strong>
-                                            <span>|</span>
-                                            <a class="delete" onclick="javascript:return confirm('<?php _e('This action can not be undone. Are you sure you want to continue?', 'modern'); ?>')" href="<?php echo osc_item_delete_url();?>" ><?php _e('Delete', 'modern'); ?></a>
-                                            <?php if(osc_item_is_inactive()) {?>
-                                            <span>|</span>
-                                            <a href="<?php echo osc_item_activate_url();?>" ><?php _e('Activate', 'modern'); ?></a>
-                                            <?php } ?>
-                                        </p>
-                                        <br />
-                                </div>
-                        <?php } ?>
-                        <br />
-                        <div class="paginate" >
-                        <?php for($i = 0 ; $i < osc_list_total_pages() ; $i++) {
-                            if($i == osc_list_page()) {
-                                printf('<a class="searchPaginationSelected" href="%s">%d</a>', osc_user_list_items_url($i), ($i + 1));
-                            } else {
-                                printf('<a class="searchPaginationNonSelected" href="%s">%d</a>', osc_user_list_items_url($i), ($i + 1));
-                            }
-                        } ?>
-                        </div>
-                    <?php } ?>
-                </div>
+        <?php osc_current_web_theme_path('header.php') ; ?>
+        <div class="content user_account">
+            <h1>
+                <strong><?php _e('User account manager', 'modern') ; ?></strong>
+            </h1>
+            <div id="sidebar">
+                <?php echo osc_private_user_menu() ; ?>
             </div>
-            <?php osc_current_web_theme_path('footer.php') ; ?>
+            <div id="main">
+                <h2><?php _e('Your items', 'modern'); ?> <a href="<?php echo osc_item_post_url() ; ?>">+ <?php _e('Post a new item', 'modern'); ?></a></h2>
+                <?php if(osc_count_items() == 0) { ?>
+                    <h3><?php _e('You don\'t have any items yet', 'modern'); ?></h3>
+                <?php } else { ?>
+                    <?php while(osc_has_items()) { ?>
+                            <div class="item" >
+                                    <h3>
+                                        <a href="<?php echo osc_item_url(); ?>"><?php echo osc_item_title(); ?></a>
+                                    </h3>
+                                    <p>
+                                    <?php _e('Publication date', 'modern') ; ?>: <?php echo osc_format_date(osc_item_pub_date()) ; ?><br />
+                                    <?php if( osc_price_enabled_at_items() ) { _e('Price', 'modern') ; ?>: <?php echo osc_format_price(osc_item_price()); } ?>
+                                    </p>
+                                    <p class="options">
+                                        <strong><a href="<?php echo osc_item_edit_url(); ?>"><?php _e('Edit', 'modern'); ?></a></strong>
+                                        <span>|</span>
+                                        <a class="delete" onclick="javascript:return confirm('<?php _e('This action can not be undone. Are you sure you want to continue?', 'modern'); ?>')" href="<?php echo osc_item_delete_url();?>" ><?php _e('Delete', 'modern'); ?></a>
+                                        <?php if(osc_item_is_inactive()) {?>
+                                        <span>|</span>
+                                        <a href="<?php echo osc_item_activate_url();?>" ><?php _e('Activate', 'modern'); ?></a>
+                                        <?php } ?>
+                                    </p>
+                                    <br />
+                            </div>
+                    <?php } ?>
+                    <br />
+                    <div class="paginate" >
+                    <?php for($i = 0 ; $i < osc_list_total_pages() ; $i++) {
+                        if($i == osc_list_page()) {
+                            printf('<a class="searchPaginationSelected" href="%s">%d</a>', osc_user_list_items_url($i), ($i + 1));
+                        } else {
+                            printf('<a class="searchPaginationNonSelected" href="%s">%d</a>', osc_user_list_items_url($i), ($i + 1));
+                        }
+                    } ?>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
-        <?php osc_show_flash_message() ; ?>
-        <?php osc_run_hook('footer'); ?>
+        <?php osc_current_web_theme_path('footer.php') ; ?>
     </body>
 </html>
