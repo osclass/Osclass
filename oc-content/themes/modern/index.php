6f0b60721c3efa2d4dc20f30bd98edf546e519bc
Carlos <garciademarina@gmail.com>
2012-05-09 15:59:46 +0200

    updated theme modern version to future 3.0

--- oc-content/themes/modern/index.php
+++ oc-content/themes/modern/index.php
@@ -3,7 +3,7 @@
 Theme Name: OSClass Modern Theme
 Theme URI: http://www.osclass.org/
 Description: This is the OSClass modern theme
-Version: 2.3
+Version: 3.0
 Author: OSClass team
 Author URI: http://www.osclass.org/
 Widgets: header,footer
