d63ef116f8e60d4ebdaa32f9bf55fcfffa147a3a
Emmanuel <emanwebdev@gmail.com>
2012-05-12 11:08:07 +0300

    Javascript

--- oc-admin/themes/modern/categories/index.php
+++ oc-admin/themes/modern/categories/index.php
@@ -272,7 +272,7 @@
                 </div>
                 <?php osc_show_flash_message('admin') ; ?>
                 <div class="jsMessage FlashMessage info" style="display: none;">
-                    <a class="close" href="void() ;">×</a>
+                    <a class="close" href="javascript:void();">×</a>
                     <p></p>
                 </div>
                 <!-- categories form -->
