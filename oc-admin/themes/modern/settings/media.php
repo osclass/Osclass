c86fdae97c4871827a2d2e1924eff41ab8ba6469
conejoninja <conejo@theninjabunny.com>
2012-05-15 16:59:30 +0200

    issue #321 watermark is propertly handled now

--- oc-admin/themes/modern/settings/media.php
+++ oc-admin/themes/modern/settings/media.php
@@ -274,6 +274,10 @@
                                         <td class="labeled"><?php _e('Image'); ?></td>
                                         <td>
                                             <input type="file" name="watermark_image"/>
+                                            <?php if(osc_is_watermark_image()!='') { ?>
+                                                <div class="help-box"><img width="100px" src="<?php echo osc_base_url()."oc-content/uploads/watermark.png" ?>" /></div>
+                                            <?php }; ?>
+                                            <div class="help-box"><?php _e("It has to be a .PNG image") ; ?></div>
                                             <div class="help-box"><?php _e("OSClass doesn't check the watermark image size") ; ?></div>
                                         </td>
                                     </tr>
