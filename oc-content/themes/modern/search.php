6373e539f8481a3e25795bac56feaa0e2c5b4163
Emmanuel <emanwebdev@gmail.com>
2012-05-18 15:39:38 +0300

    HTML escaping + XHTML fixes

--- oc-content/themes/modern/search.php
+++ oc-content/themes/modern/search.php
@@ -236,13 +236,13 @@
                         <fieldset class="box location">
                             <h3><strong><?php _e('Your search', 'modern'); ?></strong></h3>
                             <div class="row one_input">
-                                <input type="text" name="sPattern"  id="query" value="<?php echo osc_search_pattern() ; ?>" />
+                                <input type="text" name="sPattern" id="query" value="<?php echo osc_esc_html( osc_search_pattern() ); ?>" />
                                 <div id="search-example"></div>
                             </div>
                             <h3><strong><?php _e('Location', 'modern') ; ?></strong></h3>
                             <div class="row one_input">
                                 <h6><?php _e('City', 'modern'); ?></h6>
-                                <input type="text" id="sCity" name="sCity" value="<?php echo osc_search_city() ; ?>" />
+                                <input type="text" id="sCity" name="sCity" value="<?php echo osc_esc_html( osc_search_city() ); ?>" />
                             </div>
                         </fieldset>
 
