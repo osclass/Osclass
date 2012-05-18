97e32ce4ba3036864c729df312c922c5da3729c3
Emmanuel <emanwebdev@gmail.com>
2012-05-18 15:33:15 +0300

    HTML escaping + XHTML fixes + ? need to sanitize email?

--- oc-content/themes/modern/item.php
+++ oc-content/themes/modern/item.php
@@ -152,7 +152,7 @@
                                 <input type="hidden" name="page" value="item" />
                                 <input type="hidden" name="id" value="<?php echo osc_item_id() ; ?>" />
                                 <?php if(osc_is_web_user_logged_in()) { ?>
-                                    <input type="hidden" name="authorName" value="<?php echo osc_logged_user_name(); ?>" />
+                                    <input type="hidden" name="authorName" value="<?php echo osc_esc_html( osc_logged_user_name() ); ?>" />
                                     <input type="hidden" name="authorEmail" value="<?php echo osc_logged_user_email();?>" />
                                 <?php } else { ?>
                                     <label for="authorName"><?php _e('Your name', 'modern') ; ?>:</label> <?php CommentForm::author_input_text(); ?><br />
