c17085f3481afab561c437a6b350de228cb66cce
conejoninja <conejo@theninjabunny.com>
2012-05-21 11:51:18 +0200

    hPlugins not being loaded

--- oc-includes/osclass/install.php
+++ oc-includes/osclass/install.php
@@ -43,6 +43,7 @@ require_once LIB_PATH . 'osclass/helpers/hErrors.php';
 require_once LIB_PATH . 'osclass/helpers/hLocale.php';
 require_once LIB_PATH . 'osclass/helpers/hPreference.php';
 require_once LIB_PATH . 'osclass/helpers/hSearch.php';
+require_once LIB_PATH . 'osclass/helpers/hPlugins.php';
 require_once LIB_PATH . 'osclass/helpers/hTranslations.php';
 require_once LIB_PATH . 'osclass/helpers/hSanitize.php';
 require_once LIB_PATH . 'osclass/default-constants.php';
