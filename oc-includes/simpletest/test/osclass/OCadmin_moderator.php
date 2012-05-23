5834732ccebee4ff16d6165b621f178f00fa8db9
Juan Ramón <juanramon.diaz@gmail.com>
2012-05-23 13:42:22 +0200

    test issue-324-rev1

--- oc-includes/simpletest/test/osclass/OCadmin_moderator.php
+++ oc-includes/simpletest/test/osclass/OCadmin_moderator.php
@@ -3,8 +3,8 @@ require_once dirname(__FILE__).'/../../../../oc-load.php';
 
 //require_once('FrontendTest.php');
 
-class OCadmin_moderator extends OCadminTest {
-    
+class OCadmin_moderator extends OCadminTest
+{
     /*
      * Edit and email / alert
      */
@@ -19,36 +19,41 @@ class OCadmin_moderator extends OCadminTest {
             's_email' => 'testing+moderator@osclass.org',
             'b_moderator' => 1
         ));
-        
+
         $this->loginWith( 'testmoderator', $this->_password) ;
-        
+
         $this->selenium->open( osc_admin_base_url(true) );
         $this->selenium->waitForPageToLoad("2000");
         // check Admin Menu
         $res = $this->selenium->getXpathCount("//ul[@class='oscmenu']/li");
         $this->assertEqual(3, $res, "3 Menu options");
-        
+
         $res = $this->selenium->getXpathCount("//ul[@class='oscmenu']/li[@id='menu_dash']/ul/li");
         $this->assertEqual(0, $res, "0 Submenu options under id=menu_dash");
-        
+
         $res = $this->selenium->getXpathCount("//ul[@class='oscmenu']/li[@id='menu_items']/ul/li");
         $this->assertEqual(4, $res, "4 Submenu options under id=menu_items");
-        
+
         $res = $this->selenium->getXpathCount("//ul[@class='oscmenu']/li[@id='menu_users']/ul/li");
         $this->assertEqual(1, $res, "1 Submenu options under id=menu_users");
-        
+
         // try to enter to restricted zone
         $this->selenium->open(osc_admin_base_url(true).'?page=admins');
         $this->selenium->waitForPageToLoad("2000");
         $this->assertTrue($this->selenium->isTextPresent("You don't have enough permissions"), "Don't have enough permissions" ) ;
-        
+
         $this->selenium->open(osc_admin_base_url(true).'?page=items&action=settings');
         $this->selenium->waitForPageToLoad("2000");
         $this->assertTrue($this->selenium->isTextPresent("You don't have enough permissions"), "Don't have enough permissions" ) ;
-        
+
+        $this->logout();
+        $this->selenium->open(osc_admin_base_url(true).'?page=settings');
+        $this->selenium->waitForPageToLoad("2000");
+        $this->assertFalse($this->selenium->isTextPresent("You don't have enough permissions"), "Don't show the text: 'Don't have enough permissions'" ) ;
+
         // remove user testmoderator!
         Admin::newInstance()->delete(array('s_username' => 'testmoderator') );
     }
-    
 }
+
 ?>
