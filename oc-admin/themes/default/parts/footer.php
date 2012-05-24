a24ac04ea29e7f27f196d89c48656c7de0de2f57
Carlos <garciademarina@gmail.com>
2012-05-24 15:20:17 +0200

    fixed js url

--- oc-admin/themes/default/parts/footer.php
+++ oc-admin/themes/default/parts/footer.php
@@ -6,7 +6,7 @@
     <!-- javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.min.js"></script>
+    <script src="<?php echo osc_current_admin_theme_js_url('jquery.min.js') ; ?>"></script>
     <script>
     $(function(){
         $('#sidebar ul.oscmenu > li > ul').each(function(){
