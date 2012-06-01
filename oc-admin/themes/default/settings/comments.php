8311c7bbbed2be40ccae65e3639c5799ac8c5d33
Carlos <garciademarina@gmail.com>
2012-06-01 13:08:07 +0200

    idented html, minor changes

--- oc-admin/themes/default/settings/comments.php
+++ oc-admin/themes/default/settings/comments.php
@@ -84,64 +84,71 @@ $(document).ready(function(){
     }
 ?>
 <?php osc_current_admin_theme_path( 'parts/header.php' ) ; ?>
-<div id="general-setting">
-    <!-- settings form -->
-                    <div id="general-settings">
-                        <h2 class="render-title"><?php _e('Comments Settings') ; ?></h2>
-                            <ul id="error_list" style="display: none;"></ul>
-                            <form name="comments_form" action="<?php echo osc_admin_base_url(true) ; ?>" method="post">
-                                <input type="hidden" name="page" value="settings" />
-                                <input type="hidden" name="action" value="comments_post" />
-                                <fieldset>
-                                    <div class="form-horizontal">
-                                    <div class="form-row">
-                                        <div class="form-label"><?php _e('Default comment settings') ; ?></div>
-                                        <div class="form-controls">
-                                            <div class="form-label-checkbox"><input type="checkbox" <?php echo ( osc_comments_enabled() ? 'checked="checked"' : '' ) ; ?> name="enabled_comments" value="1" /><?php _e('Allow people to post comments on listings') ; ?></div>
-                                        <div class="form-label-checkbox"><input type="checkbox" <?php echo ( osc_reg_user_post_comments() ? 'checked="checked"' : '' ) ; ?> name="reg_user_post_comments" value="1" />
-                                            <?php _e('Users must be registered and logged in to comment') ; ?></div>
-                                        <div class="form-label-checkbox"><input type="checkbox" <?php echo ( ( osc_moderate_comments() == -1 ) ? '' : 'checked="checked"' ) ; ?> name="moderate_comments" value="1" />
-                                            <?php _e('A comment is held for moderation') ; ?></div>
-                                        <div class="form-label-checkbox-offset">
-                                            <?php printf( __('Before a comment appears, comment author must have %s previously approved comment'), '<input type="text" class="input-small" name="num_moderate_comments" value="' . ( (osc_moderate_comments() == -1 ) ? '' : osc_esc_html( osc_moderate_comments() ) ) . '" />' ) ; ?>
-                                            <div class="help-box"><?php _e('If the value is zero an administrator must always approve the comment') ; ?></div>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-row">
-                                        <div class="form-label"><?php _e('Other comment settings') ; ?></div>
-                                        <div class="form-controls">
-                                                <?php printf( __('Break comments into pages with %s comments per page'), '<input type="text" class="input-small" name="comments_per_page" value="' . osc_esc_html( osc_comments_per_page() ) . '" />' ) ; ?>
-                                                <div class="help-box"><?php _e('If the value is zero all the comments are shown' ) ; ?></div>
-                                        </div>
-                                    </div>
-                                    <h2 class="render-title"><?php _e('Notifications') ; ?></h2>
-                                    <div class="form-row">
-                                        <div class="form-label"><?php _e('E-mail admin whenever') ?></div>
-                                        <div class="form-controls">
-                                            <div class="form-label-checkbox">
-                                                <input type="checkbox" <?php echo ( osc_notify_new_comment() ? 'checked="checked"' : '' ) ; ?> name="notify_new_comment" value="1" />
-                                                <?php _e("There is a new comment") ; ?>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-row">
-                                        <div class="form-label"><?php _e('E-mail user whenever') ?></div>
-                                        <div class="form-controls">
-                                            <div class="form-label-checkbox">
-                                                <input type="checkbox" <?php echo ( osc_notify_new_comment_user() ? 'checked="checked"' : '' ) ; ?> name="notify_new_comment_user" value="1" />
-                                                <?php _e("There is a new comment in his listing") ; ?>
-                                            </div>
-                                        </div>
-                                    </div>
 
-                                    <div class="form-actions">
-                                        <input type="submit" value="<?php echo osc_esc_html( __('Save changes') ) ; ?>" class="btn btn-submit" />
-                                    </div>
-                                </div>
-                        </fieldset>
-                    </form>
+<div id="general-settings">
+    <ul id="error_list" style="display: none;"></ul>
+    <form name="comments_form" action="<?php echo osc_admin_base_url(true) ; ?>" method="post">
+        <input type="hidden" name="page" value="settings" />
+        <input type="hidden" name="action" value="comments_post" />
+        <fieldset>
+            <div class="form-horizontal">
+                <h2 class="render-title"><?php _e('Comments Settings') ; ?></h2>
+
+                <div class="form-row">
+                    <div class="form-label"><?php _e('Default comment settings') ; ?></div>
+                    <div class="form-controls">
+                        <div class="form-label-checkbox">
+                            <input type="checkbox" <?php echo ( osc_comments_enabled() ? 'checked="checked"' : '' ) ; ?> name="enabled_comments" value="1" /><?php _e('Allow people to post comments on listings') ; ?>
+                        </div>
+                        <div class="form-label-checkbox">
+                            <input type="checkbox" <?php echo ( osc_reg_user_post_comments() ? 'checked="checked"' : '' ) ; ?> name="reg_user_post_comments" value="1" />
+                            <?php _e('Users must be registered and logged in to comment') ; ?>
+                        </div>
+                        <div class="form-label-checkbox"><input type="checkbox" <?php echo ( ( osc_moderate_comments() == -1 ) ? '' : 'checked="checked"' ) ; ?> name="moderate_comments" value="1" />
+                            <?php _e('A comment is held for moderation') ; ?>
+                        </div>
+                        <div class="form-label-checkbox-offset">
+                            <?php printf( __('Before a comment appears, comment author must have %s previously approved comment'), '<input type="text" class="input-small" name="num_moderate_comments" value="' . ( (osc_moderate_comments() == -1 ) ? '' : osc_esc_html( osc_moderate_comments() ) ) . '" />' ) ; ?>
+                            <div class="help-box"><?php _e('If the value is zero an administrator must always approve the comment') ; ?></div>
+                        </div>
+                    </div>
                 </div>
-                <!-- /settings form -->
+                <div class="form-row">
+                    <div class="form-label"><?php _e('Other comment settings') ; ?></div>
+                    <div class="form-controls">
+                        <?php printf( __('Break comments into pages with %s comments per page'), '<input type="text" class="input-small" name="comments_per_page" value="' . osc_esc_html( osc_comments_per_page() ) . '" />' ) ; ?>
+                        <div class="help-box"><?php _e('If the value is zero all the comments are shown' ) ; ?></div>
+                    </div>
+                </div>
+
+                <h2 class="render-title"><?php _e('Notifications') ; ?></h2>
+
+                <div class="form-row">
+                    <div class="form-label"><?php _e('E-mail admin whenever') ?></div>
+                    <div class="form-controls">
+                        <div class="form-label-checkbox">
+                            <input type="checkbox" <?php echo ( osc_notify_new_comment() ? 'checked="checked"' : '' ) ; ?> name="notify_new_comment" value="1" />
+                            <?php _e("There is a new comment") ; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-label"><?php _e('E-mail user whenever') ?></div>
+                    <div class="form-controls">
+                        <div class="form-label-checkbox">
+                            <input type="checkbox" <?php echo ( osc_notify_new_comment_user() ? 'checked="checked"' : '' ) ; ?> name="notify_new_comment_user" value="1" />
+                            <?php _e("There is a new comment in his listing") ; ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-actions">
+                    <input type="submit" value="<?php echo osc_esc_html( __('Save changes') ) ; ?>" class="btn btn-submit" />
+                </div>
+
+            </div>
+        </fieldset>
+    </form>
 </div>
+                <!-- /settings form -->
 <?php osc_current_admin_theme_path( 'parts/footer.php' ) ; ?>                
