028fdbfcf3676cb8115e4a7dc4dd4b8c714b580c
Carlos <garciademarina@gmail.com>
2012-06-11 10:03:33 +0200

    implemented pagination at emails, added frm & index at emails folders (theme default)

---
+++ oc-admin/themes/default/emails/frm.php
@@ -0,0 +1,80 @@
+<?php
+    /**
+     * OSClass – software for creating and publishing online classified advertising platforms
+     *
+     * Copyright (C) 2010 OSCLASS
+     *
+     * This program is free software: you can redistribute it and/or modify it under the terms
+     * of the GNU Affero General Public License as published by the Free Software Foundation,
+     * either version 3 of the License, or (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+     * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+     * See the GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public
+     * License along with this program. If not, see <http://www.gnu.org/licenses/>.
+     */
+       
+    function customPageHeader(){ ?>
+        <h1><?php _e('Page'); ?></h1>
+<?php
+    }
+    osc_add_hook('admin_page_header','customPageHeader');
+    //customize Head
+    function customHead() { ?>
+        <script type="text/javascript" src="<?php echo osc_current_admin_theme_js_url('tiny_mce/tiny_mce.js') ; ?>"></script>
+        <script type="text/javascript">
+            tinyMCE.init({
+                mode : "textareas",
+                theme : "advanced",
+                skin: "cirkuit",
+                width: "100%",
+                height: "340px",
+                theme_advanced_buttons3 : "",
+                theme_advanced_toolbar_align : "left",
+                theme_advanced_toolbar_location : "top",
+                plugins : "media",
+                entity_encoding : "raw",
+                theme_advanced_buttons1_add : "media",
+                theme_advanced_disable : "styleselect"
+            }) ;
+        </script>
+        <?php
+    }
+    osc_add_hook('admin_header','customHead');
+    
+    $email   = __get("email") ;
+    $locales = OSCLocale::newInstance()->listAllEnabled() ;
+?>
+
+<?php osc_current_admin_theme_path('parts/header.php') ; ?>
+<h2 class="render-title"><?php _e('Edit email template'); ?></h2>
+<div id="-form">
+    <?php printLocaleTabs(); ?>
+     <form action="<?php echo osc_admin_base_url(true); ?>" method="post">
+        <input type="hidden" name="page" value="emails" />
+        <input type="hidden" name="action" value="edit_post" />
+        <?php PageForm::primary_input_hidden($email); ?>
+        <div id="left-side">
+
+            <?php printLocaleTitlePage($locales, $page); ?>
+
+            <div>
+                <label><?php _e('Internal name') ; ?></label>
+                <?php PageForm::internal_name_input_text($email) ; ?>
+                <div class="flashmessage flashmessage-warning flashmessage-inline">
+                    <p><?php _e('Used to identify the email template') ; ?></p>
+                </div>
+            </div>
+            <div class="input-description-wide">
+                <?php printLocaleDescriptionPage($locales, $page); ?>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="form-actions">
+            <input type="submit" value="" class="btn btn-submit" />
+        </div>
+    </form>
+</div>
+<?php osc_current_admin_theme_path('parts/footer.php') ; ?>
