1499a82ceaf0ae89f89363a6012d6e5e0146ab66
Juan Ramón <juanramon.diaz@gmail.com>
2012-06-11 18:01:04 +0200

    Added two new settings to the theme: - Remove link from the footer - Customize search placeholder

--- oc-content/themes/modern/functions.php
+++ oc-content/themes/modern/functions.php
@@ -19,6 +19,8 @@
      * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
 
+    define('MODERN_THEME_VERSION', '3.0');
+
     if( !OC_ADMIN ) {
         if( !function_exists('add_close_button_action') ) {
             function add_close_button_action(){
@@ -34,25 +36,33 @@
 
     function theme_modern_actions_admin() {
         switch( Params::getParam('action_specific') ) {
+            case('settings'):
+                $footerLink = Params::getParam('footer_link');
+                osc_set_preference('keyword_placeholder', Params::getParam('keyword_placeholder'), 'modern_theme');
+                osc_set_preference('footer_link', ($footerLink ? '1' : '0'), 'modern_theme');
+
+                osc_add_flash_ok_message(__('Theme settings updated correctly', 'modern'), 'admin');
+                header('Location: ' . osc_admin_render_theme_url('oc-content/themes/modern/admin/settings.php')); exit;
+            break;
             case('upload_logo'):
                 $package = Params::getFiles('logo');
                 if( $package['error'] == UPLOAD_ERR_OK ) {
                     if( move_uploaded_file($package['tmp_name'], WebThemes::newInstance()->getCurrentThemePath() . "images/logo.jpg" ) ) {
-                        osc_add_flash_ok_message(_m('The logo image has been uploaded correctly'), 'admin');
+                        osc_add_flash_ok_message(__('The logo image has been uploaded correctly', 'modern'), 'admin');
                     } else {
-                        osc_add_flash_error_message(_m("An error has occurred, please try again"), 'admin');
+                        osc_add_flash_error_message(__("An error has occurred, please try again", 'modern'), 'admin');
                     }
                 } else {
-                    osc_add_flash_error_message(_m("An error has occurred, please try again"), 'admin');
+                    osc_add_flash_error_message(__("An error has occurred, please try again", 'modern'), 'admin');
                 }
                 header('Location: ' . osc_admin_render_theme_url('oc-content/themes/modern/admin/header.php')); exit;
             break;
             case('remove'):
                 if(file_exists( WebThemes::newInstance()->getCurrentThemePath() . "images/logo.jpg" ) ) {
                     @unlink( WebThemes::newInstance()->getCurrentThemePath() . "images/logo.jpg" );
-                    osc_add_flash_ok_message(_m('The logo image has been removed'), 'admin');
+                    osc_add_flash_ok_message(__('The logo image has been removed', 'modern'), 'admin');
                 } else {
-                    osc_add_flash_error_message(_m("Image not found"), 'admin');
+                    osc_add_flash_error_message(__("Image not found", 'modern'), 'admin');
                 }
                 header('Location: ' . osc_admin_render_theme_url('oc-content/themes/modern/admin/header.php')); exit;
             break;
@@ -60,16 +70,37 @@
     }
     osc_add_hook('init_admin', 'theme_modern_actions_admin');
     osc_admin_menu_appearance(__('Header logo', 'modern'), osc_admin_render_theme_url('oc-content/themes/modern/admin/header.php'), 'header_modern');
+    osc_admin_menu_appearance(__('Theme settings', 'modern'), osc_admin_render_theme_url('oc-content/themes/modern/admin/settings.php'), 'settings_modern');
 
     if( !function_exists('logo_header') ) {
         function logo_header() {
-             $html = '<img border="0" alt="' . osc_page_title() . '" src="' . osc_current_web_theme_url('images/logo.jpg') . '" />';
-             if( file_exists( WebThemes::newInstance()->getCurrentThemePath() . "images/logo.jpg" ) ) {
+            $html = '<img border="0" alt="' . osc_page_title() . '" src="' . osc_current_web_theme_url('images/logo.jpg') . '" />';
+            if( file_exists( WebThemes::newInstance()->getCurrentThemePath() . "images/logo.jpg" ) ) {
                 return $html;
-             } else {
+            } else {
                 return osc_page_title();
             }
         }
     }
+
+    // install update options
+    if( !function_exists('modern_theme_install') ) {
+        function modern_theme_install() {
+            osc_set_preference('keyword_placeholder', __('ie. PHP Programmer', 'modern'), 'modern_theme');
+            osc_set_preference('version', MODERN_THEME_VERSION, 'modern_theme');
+            osc_set_preference('footer_link', true, 'modern_theme');
+        }
+    }
+
+    if(!function_exists('check_install_modern_theme')) {
+        function check_install_modern_theme() {
+            $current_version = osc_get_preference('version', 'modern_theme');
+            //check if current version is installed or need an update<
+            if( !$current_version ) {
+                modern_theme_install();
+            }
+        }
+    }
+    check_install_modern_theme();
 
 ?>
