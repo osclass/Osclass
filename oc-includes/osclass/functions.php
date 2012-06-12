cd2e7b2ff5a334816eaa979c3141ed299c5a984a
Carlos <garciademarina@gmail.com>
2012-06-12 13:24:37 +0200

    minor improvements on admin toolbar

--- oc-includes/osclass/functions.php
+++ oc-includes/osclass/functions.php
@@ -374,14 +374,16 @@ function osc_admin_toolbar_logout()
 function osc_admin_toolbar_comments()
 {   
     $total = ItemComment::newInstance()->countAll( array('b_active' => 0) );
-    $title = '<i class="circle circle-green">'.$total.'</i>'.__('New comments');
-    
-    AdminToolbar::newInstance()->add_menu( 
-            array('id'    => 'comments',
-                  'title' => $title,
-                  'href'  => osc_admin_base_url(true) . "?page=comments",
-                  'meta'  => array('class' => 'action-btn action-btn-black')
-            ) );
+    if( $total > 0 ) {
+        $title = '<i class="circle circle-green">'.$total.'</i>'.__('New comments');
+
+        AdminToolbar::newInstance()->add_menu( 
+                array('id'    => 'comments',
+                      'title' => $title,
+                      'href'  => osc_admin_base_url(true) . "?page=comments",
+                      'meta'  => array('class' => 'action-btn action-btn-black')
+                ) );
+    }
 }
 // and we hook our function via
 //osc_add_hook( 'add_admin_toolbar_menus', 'osc_admin_toolbar_comments') ;
@@ -389,15 +391,19 @@ function osc_admin_toolbar_comments()
 function osc_admin_toolbar_spam()
 {   
     $total = Item::newInstance()->countByMarkas( 'spam' );
-    $title = '<i class="circle circle-gray">'.$total.'</i>'.__('Spam');
-    
-    AdminToolbar::newInstance()->add_menu( 
-            array('id'    => 'spam',
-                  'title' => $title,
-                  'href'  => osc_admin_base_url(true) . "?page=items",
-                  'meta'  => array('class' => 'action-btn action-btn-black')
-            ) );
+    if( $total > 0 ) {
+        $title = '<i class="circle circle-red">'.$total.'</i>'.__('Spam');
+
+        AdminToolbar::newInstance()->add_menu( 
+                array('id'    => 'spam',
+                      'title' => $title,
+                      'href'  => osc_admin_base_url(true) . "?page=items",
+                      'meta'  => array('class' => 'action-btn action-btn-black')
+                ) );
+    }
 }
 // and we hook our function via
 //osc_add_hook( 'add_admin_toolbar_menus', 'osc_admin_toolbar_spam') ;
+
+
 ?>
