08987160cbef08c57f09936266c954956e212db3
Carlos <garciademarina@gmail.com>
2012-07-03 17:56:19 +0200

    removed extra debug

--- oc-admin/index.php
+++ oc-admin/index.php
@@ -24,56 +24,6 @@
 
     require_once ABS_PATH . 'oc-load.php' ;
 
-    
-    
-    function process_error_backtrace($errno, $errstr, $errfile, $errline, $errcontext) {
-    if(!(error_reporting() & $errno))
-        return;
-    switch($errno) {
-    case E_WARNING      :
-    case E_USER_WARNING :
-    case E_STRICT       :
-    case E_NOTICE       :
-    case E_USER_NOTICE  :
-        $type = 'warning';
-        $fatal = false;
-        break;
-    default             :
-        $type = 'fatal error';
-        $fatal = true;
-        break;
-    }
-    $trace = array_reverse(debug_backtrace());
-    array_pop($trace);
-    if(php_sapi_name() == 'cli') {
-        echo 'Backtrace from ' . $type . ' \'' . $errstr . '\' at ' . $errfile . ' ' . $errline . ':' . "\n";
-        foreach($trace as $item)
-            echo '  ' . (isset($item['file']) ? $item['file'] : '<unknown file>') . ' ' . (isset($item['line']) ? $item['line'] : '<unknown line>') . ' calling ' . $item['function'] . '()' . "\n";
-    } else {
-        echo '<p class="error_backtrace">' . "\n";
-        echo '  Backtrace from ' . $type . ' \'' . $errstr . '\' at ' . $errfile . ' ' . $errline . ':' . "\n";
-        echo '  <ol>' . "\n";
-        foreach($trace as $item)
-            echo '    <li>' . (isset($item['file']) ? $item['file'] : '<unknown file>') . ' ' . (isset($item['line']) ? $item['line'] : '<unknown line>') . ' calling ' . $item['function'] . '()</li>' . "\n";
-        echo '  </ol>' . "\n";
-        echo '</p>' . "\n";
-    }
-    if(ini_get('log_errors')) {
-        $items = array();
-        foreach($trace as $item)
-            $items[] = (isset($item['file']) ? $item['file'] : '<unknown file>') . ' ' . (isset($item['line']) ? $item['line'] : '<unknown line>') . ' calling ' . $item['function'] . '()';
-        $message = 'Backtrace from ' . $type . ' \'' . $errstr . '\' at ' . $errfile . ' ' . $errline . ': ' . join(' | ', $items);
-        error_log($message);
-    }
-    if($fatal)
-        exit(1);
-}
-
-set_error_handler('process_error_backtrace', E_ALL);
-    
-    
-    
-    
     $adminThemes = new AdminThemes() ;
     $functions_path = $adminThemes->getCurrentThemePath() . 'functions.php';
     if( file_exists($functions_path) ) {
