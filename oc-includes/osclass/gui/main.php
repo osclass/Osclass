d45ab780431f83d776f3e424bdf4d67ba4f69884
conejoninja <conejo@theninjabunny.com>
2012-07-16 12:36:43 +0200

    issue #429 fixed non w3c valid slugs

--- oc-includes/osclass/gui/main.php
+++ oc-includes/osclass/gui/main.php
@@ -51,11 +51,11 @@
                     ?>
                     <?php while ( osc_has_categories() ) { ?>
                         <div class="category">
-                            <h1><strong><a class="category <?php echo osc_category_slug() ; ?>" href="<?php echo osc_search_category_url() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span></strong></h1>
+                            <h1><strong><a class="category cat_<?php echo osc_category_id() ; ?>" href="<?php echo osc_search_category_url() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span></strong></h1>
                             <?php if ( osc_count_subcategories() > 0 ) { ?>
                                 <ul>
                                     <?php while ( osc_has_subcategories() ) { ?>
-                                        <li><a class="category <?php echo osc_category_slug() ; ?>" href="<?php echo osc_search_category_url() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span></li>
+                                        <li><a class="category cat_<?php echo osc_category_id() ; ?>" href="<?php echo osc_search_category_url() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span></li>
                                     <?php } ?>
                                 </ul>
                             <?php } ?>
--- oc-includes/osclass/helpers/hCategories.php
+++ oc-includes/osclass/helpers/hCategories.php
@@ -194,7 +194,7 @@
     }
     
     /**
-     * Gets the slug of the current category
+     * Gets the slug of the current category. WARNING: This slug could NOT be used as a valid W3C HTML tag attribute as it could have other characters besides [A-Za-z0-9-_]
      *
      * @param string $locale
      * @return string
