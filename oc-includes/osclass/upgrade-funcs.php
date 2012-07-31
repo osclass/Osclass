a5c5388549ac4b71b6ce5a968f55f9dc51cf2d2d
conejoninja <conejo@theninjabunny.com>
2012-07-31 16:18:12 +0200

    removing some lines from upgrade-funcs

--- oc-includes/osclass/upgrade-funcs.php
+++ oc-includes/osclass/upgrade-funcs.php
@@ -387,8 +387,6 @@ CREATE TABLE %st_item_description_tmp (
 
     if(osc_version() < 310) {
         $comm->query(sprintf("ALTER TABLE %st_alerts ADD COLUMN pk_i_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ", DB_TABLE_PREFIX));
-        $comm->query(sprintf("ALTER TABLE %st_alerts ADD COLUMN dt_date DATETIME NULL ", DB_TABLE_PREFIX));
-        $comm->query(sprintf("ALTER TABLE %st_alerts ADD COLUMN dt_unsub_date DATETIME NULL ", DB_TABLE_PREFIX));
         $comm->query(sprintf("UPDATE %st_alerts SET dt_date = '%s' ", DB_TABLE_PREFIX, date("Y-m-d H:i:s")));
     }
 
