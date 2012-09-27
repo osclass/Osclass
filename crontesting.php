1954180a2316a1e2e2280c2d6419b831669fb3b4
conejoninja <conejo@theninjabunny.com>
2012-09-27 09:14:06 +0200

    fix permission for crontesting.php

--- crontesting.php
+++ crontesting.php
@@ -3,13 +3,16 @@
     system("git checkout develop; echo $?", $rv);
     if($rv!=0) { echo "CRON FAILED"; exit; };
 
-    system("git reset --hard origin/develop; echo $?", $rv);
-    if($rv!=0) { echo "CRON FAILED"; exit; };
-
     system("git fetch origin develop; echo $?", $rv);
     if($rv!=0) { echo "CRON FAILED"; exit; };
 
+    system("git reset --hard origin/develop; echo $?", $rv);
+    if($rv!=0) { echo "CRON FAILED"; exit; };
+
     system("git checkout testing; echo $?", $rv);
+    if($rv!=0) { echo "CRON FAILED"; exit; };
+
+    system("git fetch origin testing; echo $?", $rv);
     if($rv!=0) { echo "CRON FAILED"; exit; };
 
     system("git reset --hard origin/testing; echo $?", $rv);
