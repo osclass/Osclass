fa995765b90615f2d1810b249eec3e2622e2d757
Carlos <garciademarina@gmail.com>
2012-12-04 18:20:31 +0100

    minor bug fix

--- oc-includes/osclass/UserActions.php
+++ oc-includes/osclass/UserActions.php
@@ -54,13 +54,13 @@
 
             $email_taken = $this->manager->findByEmail($input['s_email']);
             if( !$error && $email_taken != null ) {
+                osc_run_hook('register_email_taken', $input['s_email']);
                 $error = 3;
             }
 
             if(!$error && $input['s_username']!='') {
                 $username_taken = $this->manager->findByUsername($input['s_username']);
                 if( !$error && $username_taken != null ) {
-                    osc_run_hook('register_email_taken', $input['s_email']);
                     $error = 8;
                 }
                 $blacklist = explode(",", osc_username_blacklist());
