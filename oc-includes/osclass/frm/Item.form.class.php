dba6d8c728ca3236ee21e1f914c50d4a2c108ad4
conejoninja <conejo@theninjabunny.com>
2012-12-13 16:54:47 +0100

    some fixes on the two select method

--- oc-includes/osclass/frm/Item.form.class.php
+++ oc-includes/osclass/frm/Item.form.class.php
@@ -137,16 +137,14 @@
                             return false;
                         }
                         categories = window['categories_' + categoryID];
-                        if( !$.isArray(categories) ) {
+                        if( categories==null || !$.isArray(categories) ) {
                             var options = '<option value="' + categoryID + '" >' + osc.langs.no_subcategory + '</option>';
-                            $('#catId').html(options);
-                            $("#catId").next("a").find(".select-box-label").text(osc.langs.no_subcategory);
-                            return false;
-                        }
-                        var options = '<option value="' + categoryID + '" >' + osc.langs.select_subcategory + '</option>';
-                        $.each(categories, function(index, value){
-                            options += '<option value="' + value[0] + '">' + value[1] + '</option>';
-                        });
+                        } else {
+                            var options = '<option value="' + categoryID + '" >' + osc.langs.select_subcategory + '</option>';
+                            $.each(categories, function(index, value){
+                                options += '<option value="' + value[0] + '">' + value[1] + '</option>';
+                            });
+                        };
                         $('#catId').html(options);
                         $("#catId").next("a").find(".select-box-label").text(osc.langs.select_subcategory);
                         $("#catId").change();
