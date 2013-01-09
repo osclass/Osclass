834ee7abc18d08b85ba9c06942469a927d65e888
Carlos <garciademarina@gmail.com>
2013-01-09 16:37:32 +0100

    minor fix

--- oc-admin/themes/modern/market/header.php
+++ oc-admin/themes/modern/market/header.php
@@ -33,7 +33,7 @@
                 'screenshots'        => __('Screenshots'),
                 'download_manually'  => __('Download manually'),
                 'proceed_anyway'     => sprintf(__('Warning! This package is not compatible with your current version of Osclass (%s)'), $main_version),
-                'srue'               => __('Are you sure?'),
+                'sure'               => __('Are you sure?'),
                 'proceed_anyway_btn' => __('Ok, proceed anyway'),
                 'not_compatible'     => sprintf(__('Warning! This theme is not compatible with your current version of Osclass (%s)'), $main_version),
                 'themes' => array(
