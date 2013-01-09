b78f4cdd990ba7b03b72deeaad57a543ee6e5cc1
conejoninja <conejo@theninjabunny.com>
2013-01-09 14:52:34 +0100

    small fix on testing

--- oc-includes/simpletest/test/osclass/OCadmin-items.php
+++ oc-includes/simpletest/test/osclass/OCadmin-items.php
@@ -245,6 +245,7 @@ class OCadmin_items extends OCadminTest {
         $this->selenium->type("contactName" , "contact name");
         $this->selenium->type("contactEmail", "test@mail.com");
 
+        $this->selenium->select("parentCatId", "label=regexp:\\s*Vehicles");
         $this->selenium->select("catId", "label=regexp:\\s*Cars");
         $this->selenium->type("title[en_US]", "title item");
         $this->selenium->type("description[en_US]", "description test description test description test");
