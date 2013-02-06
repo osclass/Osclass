d8d7b539815925b466d505b939b69171e291c2a2
conejoninja <conejo@theninjabunny.com>
2013-02-06 13:06:49 +0100

    tests added to market

--- oc-includes/simpletest/test/osclass/OCadmin-market.php
+++ oc-includes/simpletest/test/osclass/OCadmin-market.php
@@ -11,7 +11,7 @@ class OCadmin_market extends OCadminTest {
     }
 
 
-    function testMarketPlugins()
+    function atestMarketPluginsPagination()
     {
 
         $this->loginWith();
@@ -21,10 +21,9 @@ class OCadmin_market extends OCadminTest {
         $this->selenium->waitForPageToLoad("10000");
 
         $text = $this->selenium->getText("//h2[@class='section-title']");
-
-        $count = $this->selenium->getCount("//a[@class='mk-item-parent']");
-        print_r($count);
-        print_r("~~~");
+        $count = $this->selenium->getXpathCount("//div[@class='mk-item mk-item-plugin']");
+        $this->assertTrue(($count==9), "Correct number of market items");
+        $p1 = $this->getPluginName();
 
         if(preg_match('|([0-9]+) plugins|', $text, $match)) {
             $last = $this->selenium->getText("css=a[class=searchPaginationNonSelected]:last");
@@ -32,15 +31,28 @@ class OCadmin_market extends OCadminTest {
             $this->selenium->click("css=a[class=searchPaginationNonSelected]:last");
             $this->selenium->waitForPageToLoad("10000");
 
-            $count = $this->selenium->getCount("//a[@class='mk-item-parent']");
-            print_r($count);
+            $count = $this->selenium->getXpathCount("//div[@class='mk-item mk-item-plugin']");
+            $this->assertTrue(($count==($match[1]-((ceil($match[1]/9)-1)*9))), "Correct number of market items");
+            $p2 = $this->getPluginName();
+            $this->assertFalse(($p1==$p2 || strpos($p2, "OR: Element")), "Same item in both pages, page didn't changed");
 
         } else {
             $this->assertTrue(false, "preg_match 'XX plugins' failed");
         }
 
-        //$p1 = $this->selenium->getText("//a[@class='mk-item-parent']");
-        //print_r($p1);
+    }
+
+    function testMarketPluginsViewInfo()
+    {
+
+        $this->loginWith();
+        $this->selenium->open( osc_admin_base_url(true) ) ;
+        $this->selenium->waitForPageToLoad("10000");
+        $this->selenium->click("//a[@id='market_view_plugins']");
+        $this->selenium->waitForPageToLoad("10000");
+
+        $text = $this->getPluginName();+
+        $this->assertFalse(strpos($text, "OR: Element"), "Market : View info failed");
 
     }
 
@@ -49,6 +61,14 @@ class OCadmin_market extends OCadminTest {
         osc_set_preference('marketURL', 'http://market.osclass.org/api/');
     }
 
+
+    private function getPluginName() {
+        $this->selenium->click("//div[@class='mk-item mk-item-plugin']/div/div/span[@class='more']");
+        sleep(1);
+        $text =  $this->selenium->getText("//div[@class='mk-inffo']/table/tbody/tr/td/h3");
+        $this->selenium->click("link=close");
+        return $text;
+    }
 
     private function deletePlugin() {
         @chmod(CONTENT_PATH."plugins/breadcrumbs/index.php", 0777);
