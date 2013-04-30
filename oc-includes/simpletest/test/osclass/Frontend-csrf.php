e4aeb3b9619565178861c2e65e1df70f7ee797df
Carlos <garciademarina@gmail.com>
2013-04-30 10:38:01 +0200

    added crsf testing

---
+++ oc-includes/simpletest/test/osclass/Frontend-csrf.php
@@ -0,0 +1,44 @@
+<?php
+require_once dirname(__FILE__).'/../../../../oc-load.php';
+
+//require_once('FrontendTest.php');
+
+class Frontend_csrf extends FrontendTest {
+
+    /*
+     *
+     */
+    function testCsrfRedirect()
+    {
+        // Probable invalid request
+        $this->selenium->open( osc_base_url(true) . '?page=login' );
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFName')[0].value = ''; }");
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFToken')[0].value = ''; }");
+
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFName')[1].value = ''; }");
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFToken')[1].value = ''; }");
+
+        $this->selenium->click("xpath=//span/button[text()='Log in']");
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->assertTrue($this->selenium->isTextPresent("Probable invalid request"), 'Testing, CSRFName, CSRFToken empty.');
+
+        // Invalid CSRF token
+        $this->selenium->open( osc_base_url(true) . '?page=login' );
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFName')[0].value = 'foo'; }");
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFToken')[0].value = 'bar'; }");
+
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFName')[1].value = 'foo'; }");
+        $this->selenium->runScript("javascript{ this.browserbot.getCurrentWindow().document.getElementsByName('CSRFToken')[1].value = 'bar'; }");
+
+        $this->selenium->click("xpath=//span/button[text()='Log in']");
+        $this->selenium->waitForPageToLoad("30000");
+
+        $this->assertTrue($this->selenium->isTextPresent("Invalid CSRF token"), 'Testing, CSRFName, CSRFToken incorrect.');
+    }
+}
+?>
