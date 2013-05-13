f345025b77d4147eeeef9da0139baeeb40f4160d
Fernando Alou <hola@fernandoalou.com>
2013-05-13 16:52:29 +0200

    replace default theme in basic_data.sql

--- oc-includes/osclass/installer/basic_data.sql
+++ oc-includes/osclass/installer/basic_data.sql
@@ -5,7 +5,7 @@ INSERT INTO /*TABLE_PREFIX*/t_currency (pk_c_code, s_name, s_description, b_enab
 
 INSERT INTO /*TABLE_PREFIX*/t_preference VALUES
     ('osclass', 'version', 312, 'INTEGER')
-    ,('osclass', 'theme', 'modern', 'STRING')
+    ,('osclass', 'theme', 'bender', 'STRING')
     ,('osclass', 'admin_language', 'en_US', 'STRING')
     ,('osclass', 'language', 'en_US', 'STRING')
     ,('osclass', 'pageDesc', '', 'STRING')
