080d904e149e4abe7e73827a25217bccef46a996
conejoninja <conejo@theninjabunny.com>
2013-05-28 09:11:04 +0200

    fix osc_set_preference in upgrade-funcs.php

--- oc-includes/osclass/upgrade-funcs.php
+++ oc-includes/osclass/upgrade-funcs.php
@@ -420,8 +420,8 @@ CREATE TABLE %st_item_description_tmp (
     }
 
     if(osc_version() < 320) {
-        osc_set_preference('osclass', 'mailserver_mail_from', '', 'STRING');
-        osc_set_preference('osclass', 'mailserver_name_from', '', 'STRING');
+        osc_set_preference('mailserver_mail_from', '');
+        osc_set_preference('mailserver_name_from', '');
 
         $comm->query(sprintf("ALTER TABLE  %st_category ADD  `b_price_enabled` TINYINT(1) NOT NULL DEFAULT 1", DB_TABLE_PREFIX));
     }
