15dcaa3f98b64c742972485a280866b9c3851885
conejoninja <conejo@theninjabunny.com>
2013-07-05 09:10:55 +0200

    small improvement

--- oc-includes/osclass/ItemActions.php
+++ oc-includes/osclass/ItemActions.php
@@ -164,6 +164,7 @@
             }
 
             // hook pre add or edit
+            // DEPRECATED: pre_item_post will be removed in 3.4
             osc_run_hook('pre_item_post');
             osc_run_hook('pre_item_add', $aItem);
 
@@ -362,6 +363,7 @@
             };
 
             // hook pre add or edit
+            // DEPRECATED : preitem_psot will be removed in 3.4
             osc_run_hook('pre_item_post');
             osc_run_hook('pre_item_edit', $aItem);
 
