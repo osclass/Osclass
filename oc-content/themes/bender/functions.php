368244ad543ce9d1420c4c2ab77af2fc4021502a
conejoninja <conejo@theninjabunny.com>
2013-08-02 10:13:19 +0200

    issue #882

--- oc-content/themes/bender/functions.php
+++ oc-content/themes/bender/functions.php
@@ -386,8 +386,7 @@ FUNCTIONS
         function delete_user_js() {
             $location = Rewrite::newInstance()->get_location();
             $section  = Rewrite::newInstance()->get_section();
-
-            if( ($location === 'user' && in_array($section, array('dashboard', 'profile', 'alerts', 'change_email', 'change_username',  'change_password', 'items'))) || ($location ==='custom' && Params::getParam('in_user_menu')==true ) ) {
+            if( ($location === 'user' && in_array($section, array('dashboard', 'profile', 'alerts', 'change_email', 'change_username',  'change_password', 'items'))) || (Params::getParam('page') ==='custom' && Params::getParam('in_user_menu')==true ) ) {
                 osc_enqueue_script('delete-user-js');
             }
         }
