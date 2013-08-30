b0c65c98a0dbe9d969dd6212d54e33629b8b65d2
conejoninja <conejo@theninjabunny.com>
2013-08-30 11:53:52 +0200

    issue #925 prevent double submit

--- oc-admin/themes/modern/items/index.php
+++ oc-admin/themes/modern/items/index.php
@@ -91,7 +91,10 @@
                     modal: true
                 });
                 $("#bulk-actions-submit").click(function() {
-                    $("#datatablesForm").submit();
+                    if($("#bulk-actions-submit").prop("clicked")==false) {
+                        $("#bulk-actions-submit").prop("clicked", true);
+                        $("#datatablesForm").submit();
+                    }
                 });
                 $("#bulk-actions-cancel").click(function() {
                     $("#datatablesForm").attr('data-dialog-open', 'false');
@@ -110,6 +113,7 @@
                     $("#dialog-bulk-actions .form-row").html($("#bulk_actions option:selected").attr('data-dialog-content'));
                     $("#bulk-actions-submit").html($("#bulk_actions option:selected").text());
                     $("#datatablesForm").attr('data-dialog-open', 'true');
+                    $("#bulk-actions-submit").prop("clicked", false);
                     $("#dialog-bulk-actions").dialog('open');
                     return false;
                 });
