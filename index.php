0cab947fabc94bac83004e24eafda6bf08936dab
groomain <romain.bresson@gmail.com>
2014-02-14 11:30:07 +0100

    BUG - custom maintenance page / issue #1338
    Hi,
    
    You need to add exit; code to the custom maintenance page in the first index.php.
    Because if they have one, process is still running . So we have displayed the custom.php and home.php.

--- index.php
+++ index.php
@@ -40,17 +40,19 @@
 
     if( file_exists(ABS_PATH . '.maintenance') ) {
         if(!osc_is_admin_user_logged_in()) {
+            
+            header('HTTP/1.1 503 Service Temporarily Unavailable');
+            header('Status: 503 Service Temporarily Unavailable');
+            header('Retry-After: 900');
+            
             if(file_exists(WebThemes::newInstance()->getCurrentThemePath().'maintenance.php')) {
                 osc_current_web_theme_path('maintenance.php');
+                die();
             } else {
                 require_once LIB_PATH . 'osclass/helpers/hErrors.php';
 
                 $title   = sprintf(__('Maintenance &raquo; %s'), osc_page_title());
                 $message = sprintf(__('We are sorry for any inconvenience. %s is undergoing maintenance.') . '.', osc_page_title() );
-
-                header('HTTP/1.1 503 Service Temporarily Unavailable');
-                header('Status: 503 Service Temporarily Unavailable');
-                header('Retry-After: 900');
                 osc_die($title, $message);
             }
         } else {
