286cd04a23cea2be3e9cdd8278dadec2a3992d6f
conejoninja <conejo@theninjabunny.com>
2014-04-06 11:31:59 +0200

    issue #1430 we should not assume that $countries[0]['s_name'] exists

--- oc-includes/osclass/frm/User.form.class.php
+++ oc-includes/osclass/frm/User.form.class.php
@@ -102,7 +102,7 @@
             if( count($countries) > 1 ) {
                 parent::generic_select('countryId', $countries, 'pk_c_code', 's_name', __('Select a country...'), (isset($user['fk_c_country_code'])) ? $user['fk_c_country_code'] : null);
             } else {
-                parent::generic_input_text('country', ( ! empty($user['s_country']) ? $user['s_country'] : $countries[0]['s_name']));
+                parent::generic_input_text('country', ( ! empty($user['s_country']) ? $user['s_country'] : @$countries[0]['s_name']));
             }
         }
 
