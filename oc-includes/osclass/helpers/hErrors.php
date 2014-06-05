3bcb4acbbc84b81ce5e8244b6a08d2dc901e966c
conejoninja <conejo@theninjabunny.com>
2014-06-05 10:45:58 +0200

    improvements

--- oc-includes/osclass/helpers/hErrors.php
+++ oc-includes/osclass/helpers/hErrors.php
@@ -37,7 +37,7 @@
             <head>
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
                 <title><?php echo $title; ?></title>
-                <link rel="stylesheet" type="text/css" media="all" href="oc-includes/osclass/installer/install.css" />
+                <link rel="stylesheet" type="text/css" media="all" href="<?php echo osc_get_absolute_url(); ?>oc-includes/osclass/installer/install.css" />
             </head>
             <body class="page-error">
                 <h1><?php echo $title; ?></h1>
