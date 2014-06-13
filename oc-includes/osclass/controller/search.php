0d48b8bf5273cf02560dfc6a7f7bd7b581e8972d
conejoninja <conejo@theninjabunny.com>
2014-06-13 12:20:57 +0200

    fix

--- oc-includes/osclass/controller/search.php
+++ oc-includes/osclass/controller/search.php
@@ -166,7 +166,7 @@
 
             $uriParams = Params::getParamsAsArray();
             $searchUri = osc_search_url($uriParams);
-            if($searchUri!=(WEB_PATH . $this->uri)) {
+            if($searchUri!=(WEB_PATH . urldecode($this->uri))) {
                 $this->redirectTo($searchUri, 301);
             }
 
