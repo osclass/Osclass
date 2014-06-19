e1ecd301330392ffb805d61eea1dce28bc15e19d
dev101-osclass <businesss.mail@gmail.com>
2014-06-19 22:55:07 +0200

    Update media.php

--- oc-admin/themes/modern/settings/media.php
+++ oc-admin/themes/modern/settings/media.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('OC_ADMIN')) exit('Direct access is not allowed.');
 /*
  * Copyright 2014 Osclass
- *n
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
