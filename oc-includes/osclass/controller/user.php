b394e55ef7933344f7eb476edb01d13d38db7af1
conejoninja <conejo@theninjabunny.com>
2014-07-01 08:43:27 +0200

    issue #1568

--- oc-includes/osclass/controller/user.php
+++ oc-includes/osclass/controller/user.php
@@ -103,7 +103,7 @@
                 break;
                 case('change_email_post'):      //change email post
                                                 osc_csrf_check();
-                                                if(!preg_match("/^[_a-z0-9-\+]+(\.[_a-z0-9-\+]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", Params::getParam('new_email'))) {
+                                                if(!osc_validate_email(Params::getParam('new_email'))) {
                                                     osc_add_flash_error_message( _m('The specified e-mail is not valid'));
                                                     $this->redirectTo( osc_change_user_email_url() );
                                                 } else {
