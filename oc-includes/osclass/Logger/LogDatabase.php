43664b00b6c08e5387d97abebcfe34e4f0500877
conejoninja <conejo@theninjabunny.com>
2014-08-06 08:42:55 +0200

    fix

--- oc-includes/osclass/Logger/LogDatabase.php
+++ oc-includes/osclass/Logger/LogDatabase.php
@@ -137,7 +137,7 @@
             }
 
             fwrite($fp, '==================================================' . PHP_EOL);
-            fwrite($fp, '=' . str_pad('Date: ' . date(osc_date_format()?osc_date_format():'Y-m-d').' '.date(osc_time_format()?osc_date_format():'H:i:s'), 48, " ", STR_PAD_BOTH) . '=' . PHP_EOL);
+            fwrite($fp, '=' . str_pad('Date: ' . date(osc_date_format()!=''?osc_date_format():'Y-m-d').' '.date(osc_time_format()!=''?osc_date_format():'H:i:s'), 48, " ", STR_PAD_BOTH) . '=' . PHP_EOL);
             fwrite($fp, '=' . str_pad('Total queries: ' . $this->getTotalNumberQueries(), 48, " ", STR_PAD_BOTH) . '=' . PHP_EOL);
             fwrite($fp, '=' . str_pad('Total queries time: ' . $this->getTotalQueriesTime(), 48, " ", STR_PAD_BOTH) . '='  . PHP_EOL);
             fwrite($fp, '==================================================' . PHP_EOL . PHP_EOL);
