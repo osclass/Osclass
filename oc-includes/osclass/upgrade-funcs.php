74978a0759508e9bd2fc884433a4c3d8e39d44cf
conejoninja <conejo@theninjabunny.com>
2014-11-13 15:51:24 +0100

    fix

--- oc-includes/osclass/upgrade-funcs.php
+++ oc-includes/osclass/upgrade-funcs.php
@@ -503,7 +503,7 @@ CREATE TABLE %st_item_description_tmp (
     }
 
     if(osc_version() < 350) {
-        osc_set_preference('marketURL', 'http://market.osclass.org.local/api/v2/');
+        osc_set_preference('marketURL', 'http://market.osclass.org/api/v2/');
         osc_set_preference('marketAPIConnect', '');
         osc_set_preference('marketCategories', '');
         osc_set_preference('marketDataUpdate', 0);
