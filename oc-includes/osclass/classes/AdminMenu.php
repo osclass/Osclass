f676db40f33ccb6a9c2a49eaa8633f1168734cfe
dev-101 <dev-101@users.noreply.github.com>
2015-03-30 15:28:30 +0200

    fix issue #1852

--- oc-includes/osclass/classes/AdminMenu.php
+++ oc-includes/osclass/classes/AdminMenu.php
@@ -49,7 +49,7 @@
         {
             $this->add_menu( __('Dashboard'), osc_admin_base_url(), 'dash', 'moderator');
 
-            $this->add_menu( __('Listing'), osc_admin_base_url(true).'?page=items', 'items', 'moderator');
+            $this->add_menu( __('Listings'), osc_admin_base_url(true).'?page=items', 'items', 'moderator');
             $this->add_submenu( 'items', __('Manage listings'), osc_admin_base_url(true).'?page=items', 'items_manage', 'moderator');
             $this->add_submenu( 'items', __('Reported listings'), osc_admin_base_url(true).'?page=items&action=items_reported', 'items_reported', 'moderator');
             $this->add_submenu( 'items', __('Manage media'), osc_admin_base_url(true).'?page=media', 'items_media', 'moderator');
