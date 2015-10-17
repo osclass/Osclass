0093be4755f88a307414b1b41052be853787995c
Daniel Esteban <conejo@conejo.me>
2015-10-17 18:16:11 +0200

    fix for php < 5.4

--- oc-includes/osclass/helpers/hErrors.php
+++ oc-includes/osclass/helpers/hErrors.php
@@ -61,10 +61,20 @@
         }
 
         if(get_magic_quotes_gpc()) {
-            $value = strip_slashes_extended($value);
+            $value = strip_slashes_extended_e($value);
         }
 
         return ($value);
+    }
+    function strip_slashes_extended_e($array) {
+        if(is_array($array)) {
+            foreach($array as $k => &$v) {
+                $v = strip_slashes_extended_e($v);
+            }
+        } else {
+            $array = stripslashes($array);
+        }
+        return $array;
     }
     function osc_get_absolute_url() {
         $protocol = (getErrorParam('HTTPS') == 'on'  || getErrorParam('HTTPS') == 1  || getErrorParam('HTTP_X_FORWARDED_PROTO')=='https')? 'https' : 'http';
