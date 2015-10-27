cfe48cbaa161efc60a65dce12434b6b690ab4fc5
dev-101 <dev-101@users.noreply.github.com>
2015-10-27 12:46:26 +0100

    language full name

--- oc-admin/gui/login.php
+++ oc-admin/gui/login.php
@@ -54,7 +54,7 @@
                     <p>
                         <select name="locale" id="user_language">
                         <?php foreach($locales as $locale) { ?>
-                            <option value="<?php echo $locale ['pk_c_code']; ?>" <?php if(osc_admin_language() == $locale['pk_c_code']) echo 'selected="selected"'; ?>><?php echo $locale['s_short_name']; ?></option>
+                            <option value="<?php echo $locale ['pk_c_code']; ?>" <?php if(osc_admin_language() == $locale['pk_c_code']) echo 'selected="selected"'; ?>><?php echo $locale['s_name']; ?></option>
                         <?php } ?>
                         </select>
                     </p>
