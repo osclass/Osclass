5b644cabf343aef145d03b4101e6c2d1afa5de7d
Daniel Esteban <conejo@conejo.me>
2016-11-25 11:01:02 +0100

    issue #1720

--- oc-includes/osclass/upgrade-funcs.php
+++ oc-includes/osclass/upgrade-funcs.php
@@ -515,7 +515,9 @@ CREATE TABLE %st_item_description_tmp (
 
     if(osc_version() < 370) {
         osc_set_preference('marketURL', 'https://market.osclass.org/api/v2/');
+        $comm->query(sprintf("ALTER TABLE  %st_category_description MODIFY s_slug VARCHAR(255) NOT NULL", DB_TABLE_PREFIX));
     }
+
     osc_changeVersionTo(370);
 
     if(!defined('IS_AJAX') || !IS_AJAX) {
