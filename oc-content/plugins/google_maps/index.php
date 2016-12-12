976bc6066b7a0fa02d558f6a14dc392a950e2446
Daniel Esteban <conejo@conejo.me>
2016-12-12 11:54:48 +0100

    bump version google maps

--- oc-content/plugins/google_maps/index.php
+++ oc-content/plugins/google_maps/index.php
@@ -3,7 +3,7 @@
 Plugin Name: Google Maps
 Plugin URI: http://www.osclass.org/
 Description: This plugin shows a Google Map on the location space of every item.
-Version: 2.1.6
+Version: 2.1.7
 Author: Osclass & kingsult
 Author URI: http://www.osclass.org/
 Plugin update URI: http://www.osclass.org/files/plugins/google_maps/update.php
