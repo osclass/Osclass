10f6a10131e5af9422660fa0fd8b0670f17062ce
Carlos <carlos@osclass.org>
2018-07-04 09:51:08 +0200

    Added Connect market link at admin menu under market section

--- oc-includes/osclass/classes/AdminMenu.php
+++ oc-includes/osclass/classes/AdminMenu.php
@@ -50,6 +50,12 @@
             $this->add_menu( __('Dashboard'), osc_admin_base_url(), 'dash', 'moderator');
 
             $this->add_menu( __('Market'), osc_admin_base_url(true) .'?page=market', 'market', 'administrator');
+            if( !osc_is_moderator() ) {
+                $connected = osc_check_market_connect();
+                if(!$connected) {
+                    $this->add_submenu( 'market', __('Connect Market'), osc_admin_base_url(true) . "?page=market&open_market_connect=true", 'market_connect', 'administrator');
+                }
+            }
             $this->add_submenu( 'market', __('Themes'), osc_admin_base_url(true) .'?page=market&action=themes', 'market_view_themes', 'administrator');
             $this->add_submenu( 'market', __('Plugins'), osc_admin_base_url(true).'?page=market&action=plugins', 'market_view_plugins', 'administrator');
             $this->add_submenu( 'market', __('Languages'), osc_admin_base_url(true).'?page=market&action=languages', 'market_view_languages', 'administrator');
