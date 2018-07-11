c1d56da832748c4dc0a39493fff2ad0dc97e070d
Carlos <carlos@osclass.org>
2018-07-11 08:39:37 +0200

    updated gui folder

--- oc-includes/osclass/gui/index.php
+++ oc-includes/osclass/gui/index.php
@@ -23,7 +23,7 @@
 Theme Name: bender
 Theme URI: http://osclass.org/
 Description: Bender theme
-Version: 3.1.4
+Version: 3.1.5
 Author: Osclass
 Author URI: http://osclass.org/
 Widgets:  header, footer
@@ -33,7 +33,7 @@ Theme update URI: bender
     function bender_theme_info() {
         return array(
              'name'        => 'bender'
-            ,'version'     => '3.1.4'
+            ,'version'     => '3.1.5'
             ,'description' => 'Bender theme'
             ,'author_name' => 'Osclass'
             ,'author_url'  => 'http://osclass.org'
