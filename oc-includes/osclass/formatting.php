dacd32598d5a7f2d053cf27bd088578efa6048c1
Tung Pham <tung.42@gmail.com>
2022-09-20 10:13:37 +0700

    Update formatting.php

--- oc-includes/osclass/formatting.php
+++ oc-includes/osclass/formatting.php
@@ -197,7 +197,24 @@ function remove_accents($string) {
 	$string = preg_replace('#&([a-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '$1', $string);
 	$string = preg_replace('#&([a-z]{2})(?:lig);#', '$1', $string);
 	$string = preg_replace('#&[^;]+;#', '', $string);
+	
+	// Remove Vietnamese Accents
+	$string = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $string);
+	$string = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $string);
+	$string = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $string);
+	$string = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $string);
+	$string = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $string);
+	$string = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $string);
+	$string = preg_replace("/(đ)/", 'd', $string);
 
+	$string = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $string);
+	$string = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $string);
+	$string = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $string);
+	$string = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $string);
+	$string = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $string);
+	$string = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $string);
+	$string = preg_replace("/(Đ)/", 'D', $string);
+	
 	return $string;
 }
 
